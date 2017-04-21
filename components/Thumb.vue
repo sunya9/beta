c296b763bdd01a927b40cfa1a4320b2946d00fc0
sunya9 <mugen.xyz@gmail.com>
2017-04-21 16:00:10 +0900

    Add thumbnail component

---
+++ components/Thumb.vue
@@ -0,0 +1,29 @@
+<template>
+  <a :href="original">
+    <img :src="thumb" alt="" class="img-thumbnail">
+  </a>
+</template>
+
+<script>
+export default {
+  props: ['original', 'thumb'],
+  mounted() {
+    if(process.BROWSER_BUILD) {
+      const Zooming = require('zooming')
+      const zooming = new Zooming({
+        bgColor: '#000',
+        zIndex: 2000,
+        bgOpacity: .5
+      })
+      zooming.listen(this.$el.querySelector('img'))
+    }
+  }
+}
+</script>
+
+<style scoped>
+.img-thumbnail {
+  max-height: 96px;
+  max-width: 96px;
+}
+</style>
