f6bbabcabf6cb570073d5e82014a9814d78924d0
sunya9 <mugen.xyz@gmail.com>
2017-04-21 16:03:38 +0900

    Improvve user item

--- components/User.vue
+++ components/User.vue
@@ -18,7 +18,7 @@
             <follow-button :initial-state="user.you_follow" :user-id="user.id" />
           </div>
         </div>
-        <p v-html="html"></p>
+        <p @click="clickPostLink" v-html="html"></p>
       </div>
     </div>
   </li>
@@ -26,15 +26,41 @@
 
 <script>
 import FollowButton from '~components/FollowButton'
+import cheerio from 'cheerio'
+import router from '~router'
+
 export default {
   props: ['data'],
   computed: {
     html() {
-      if(this.user.content && this.user.content.html)
-      return this.user.content.html
+      if(this.user.content && this.user.content.html) {
+        const $ = cheerio.load(this.user.content.html)
+        $('a').attr('target', '_new')
+        $('span[data-mention-name]')
+          .replaceWith(function () {
+            const name = $(this).data('mention-name')
+            const text = $(this).text()
+            return `<a href="/@${name}">${text}</a>`
+          })
+        $('span[data-tag-name]')
+          .replaceWith(function () {
+            const tag = $(this).data('tag-name')
+            const text = $(this).text()
+            return `<a href="/tags/${tag}">${text}</a>`
+          })
+        return $.html()
+      }
     },
     user() {
       return this.data
+    }
+  },
+  methods: {
+    clickPostLink (e) {
+      const a = e.target
+      if (!a.href || !a.getAttribute('href').startsWith('/')) return
+      e.preventDefault()
+      router.push(a.pathname)
     }
   },
   components: {
