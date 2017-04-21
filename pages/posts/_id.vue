68a9e7ace34f0918d9d9d5c99b8424478cb73739
sunya9 <mugen.xyz@gmail.com>
2017-04-21 16:03:05 +0900

    Support conversations

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <h3>Post</h3>
-    <div class="mt-3">
-      <post :data="post" />
+    <div>
+      <list :data="before" type="Post" v-if="before.data.length" :key="`${id}-before`" />
     </div>
     <div>
-      <compose />
+      <post :data="post" @post="addAfter" detail />
+    </div>
+    <div>
+      <compose focus />
+    </div>
+    <div>
+      <list :data="after" type="Post" v-if="after.data.length" :key="`${id}-after`" />
     </div>
   </div>
 </template>
@@ -13,18 +18,55 @@
 <script>
 import Post from '~components/Post'
 import Compose from '~components/Compose'
+import List from '~components/List'
 import api from '~plugins/api'
+import bus from '~assets/js/bus'
 
 export default {
   async asyncData(ctx) {
     const { params: { id } } = ctx
-    const { data: post } = await api(ctx).fetch()
+    const _api = api(ctx)
+    const postPromise = _api.fetch()
+    const beforePromise = _api.get(`/posts/${id}/thread`, {
+      before_id: id
+    })
+    const afterPromise = _api.get(`/posts/${id}/thread`, {
+      since_id: id
+    })
+    const [{ data: post }, before, after] = await Promise.all([
+      postPromise,
+      beforePromise,
+      afterPromise
+    ])
+    before.data = before.data.reverse()
+    after.data = after.data.reverse()
     return {
-      post
+      post, before, after, id
     }
   },
+  validate ({ params }) {
+    return /^\d+$/.test(params.id)
+  },
   components: {
-    Post, Compose
+    Post, Compose, List
+  },
+  mounted() {
+    bus.$emit('setReply', this.post)
+  },
+  methods: {
+    addAfter (post) {
+      this.after.push(post)
+    }
+  },
+  head() {
+    let title = this.post.content.text
+    if(title.length > 30) {
+      title = title.substr(0, 30) + '…'
+    }
+    title = `${this.post.user.name}: ${title}`
+    return {
+      title
+    }
   }
 }
 </script>
