d901d58ae86046827dcf854fc6bbd025bc2c4db8
sunya9 <mugen.xyz@gmail.com>
2017-04-21 16:01:41 +0900

    Add tag search page

---
+++ pages/tags/_name.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>#{{name}}</h3>
+    <list :data="data" type="Post" :key="name" />
+  </div>
+</template>
+
+<script>
+import api from '~plugins/api'
+import List from '~components/List'
+
+export default {
+  async asyncData(ctx) {
+    const { params } = ctx
+    const { name } = params
+    const data = await api(ctx).fetch()
+    return {
+      data,
+      name
+    }
+  },
+  components: {
+    List
+  },
+  head() {
+    title: `#${this.name}`
+  }
+}
+</script>
