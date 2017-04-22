b80d6aaeb884e6f993be66728d179ccf44d6c2ef
sunya9 <mugen.xyz@gmail.com>
2017-04-22 11:06:42 +0900

    Fixed #6 Fix missing posts in global

--- pages/global.vue
+++ pages/global.vue
@@ -2,7 +2,7 @@
   <div>
       <h3>Global</h3>
       <div>
-        <compose />
+        <compose @post="add" />
       </div>
       <div>
         <list :data="data" type="Post" />
@@ -17,12 +17,19 @@ import api from '~plugins/api'
 
 export default {
   async asyncData(ctx) {
-    const data = await api(ctx).fetch()
+    const data = await api(ctx).fetch({
+      include_directed_posts: 1
+    })
     return { data }
   },
   components: {
     List,
     Compose
+  },
+  methods: {
+    add(post) {
+      this.data.data.unshift(post)
+    }
   },
   head: {
     title: 'Global'
