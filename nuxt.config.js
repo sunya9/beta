dcea9382d0b71754129b725c0578a41d96a2965f
sunya9 <mugen.xyz@gmail.com>
2017-04-22 10:32:51 +0900

    Remove favicon declaration temporarily

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,6 @@ module.exports = {
 
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat' }
     ]
   },
