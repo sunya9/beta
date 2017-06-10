9e0d7d84ef637eaf43c8d83a91a4ee9b3cdd2b64
sunya9 <mugen.xyz@gmail.com>
2017-06-11 04:26:34 +0900

    :tada: Create share script!

---
+++ pages/about/bookmarklet.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <h1 class="h3 card-title">Share a link on pnut.io</h1>
+      <a :href="shareScript" class="btn btn-primary">Share on pnut.io</a>
+    </div>
+  </div>
+</template>
+
+<script>
+const shareScript = `
+javascript:(function(){var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,W=window;if(C>A){G=Math.round((C/2)-(A/2))}W.open('/intent/post?text='+encodeURIComponent(document.title)+'&url='+encodeURIComponent(W.location),'','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');}());
+`
+
+export default {
+  data () {
+    return {
+      shareScript
+    }
+  },
+  head: {
+    title: 'Bookmarklet'
+  }
+}
+</script>
---
+++ pages/intent/post.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="mt-5 pt-5">
+    <div v-if="!posted">
+      <h1 class="h3">Share a link</h1>
+      <div class="my-4 compose">
+        <div class="card">
+          <div>
+            <compose-inner @post="finishPosting" no-photo :initial-text="message" :focus="true" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <h1 class="h3">Shared link!</h1>
+      <button class="btn btn-primary my-2" @click="close" autofocus>Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import ComposeInner from '~components/ComposeInner'
+
+export default {
+  middleware: 'authenticated',
+  layout: 'intent',
+  async asyncData ({ query }) {
+    const { text, url } = query
+    const message = text && url ? `[${text}](${url})` : ''
+    return {
+      message, url, text, posted: false
+    }
+  },
+  methods: {
+    finishPosting () {
+      this.posted = true
+    },
+    close () {
+      window.close()
+    }
+  },
+  components: {
+    ComposeInner
+  }
+}
+
+</script>
