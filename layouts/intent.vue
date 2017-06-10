4148697cd9bd6b753f861bfc0632ddb9fa6cd9dd
sunya9 <mugen.xyz@gmail.com>
2017-06-11 05:14:34 +0900

    Add intent layout

---
+++ layouts/intent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <app-header />
+    <div class="container">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppHeader from '~components/Header'
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
