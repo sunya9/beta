ba0196f584cf46c795409cbea61282852580dba2
sunya9 <mugen.xyz@gmail.com>
2017-06-11 04:20:38 +0900

    Remove trailing comma

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'beta is pnut.io client.' },
+      { hid: 'description', name: 'description', content: 'beta is pnut.io client.' }
 
     ],
     link: [
