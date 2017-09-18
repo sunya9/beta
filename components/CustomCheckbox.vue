0a77defb5d1ea1ab93643bd7023628e35eb4d694
sunya9 <mugen.xyz@gmail.com>
2017-09-18 21:51:21 +0900

    Add custom checkbox component

---
+++ components/CustomCheckbox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="form-check"
+    :class="{
+      disabled: disabled
+    }">
+    <label class="custom-control custom-checkbox">
+    <input type="checkbox" class="custom-control-input" @change="updateValue" :disabled="disabled" :checked="checked">
+    <span class="custom-control-indicator"></span>
+    <span class="custom-control-description">
+      <slot></slot>
+    </span>
+    </label>
+  </div>
+</template>
+<script>
+export default {
+  props: ['disabled', 'checked'],
+  model: {
+    prop: 'checked',
+    event: 'change'
+  },
+  methods: {
+    updateValue (e) {
+      const { checked } = e.target
+      this.$emit('change', checked)
+    }
+  }
+}
+</script>
