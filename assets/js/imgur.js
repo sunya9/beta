a6a2d1971fc94dac69120ea51a7556df7ff665ee
sunya9 <mugen.xyz@gmail.com>
2017-09-19 18:05:12 +0900

    Remove imgur

--- assets/js/imgur.js
+++
@@ -1,8 +0,0 @@
-
-export const setToken = tokenInfo => {
-  // set expiry date
-  const storedTokenInfo = Object.assign({}, tokenInfo)
-  storedTokenInfo.expiryDate = Date.now() + storedTokenInfo.expires_in * 1000
-  const storedTokenStr = JSON.stringify(storedTokenInfo)
-  localStorage.setItem('imgur', storedTokenStr)
-}
