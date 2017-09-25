26160a465fd83fda5068379a023313d496955975
sunya9 <mugen.xyz@gmail.com>
2017-09-25 20:36:49 +0900

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,9 @@ module.exports = {
     node: true,
     mocha: true
   },
-  extends: 'standard',
+  extends: ['prettier', 'standard'],
   // required to lint *.vue files
-  plugins: [
-    'vue',
-    'chai-expect',
-    'chai-friendly'
-  ],
+  plugins: ['vue', 'chai-expect', 'chai-friendly'],
   // add your custom rules here
   rules: {
     'no-unused-expressions': 'off',
