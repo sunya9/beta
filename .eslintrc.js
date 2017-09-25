2007b16aa5968e442f82ddf00253c9ca0ca7a37c
sunya9 <mugen.xyz@gmail.com>
2017-09-25 21:51:33 +0900

    Remove prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,21 @@ module.exports = {
     node: true,
     mocha: true
   },
-  extends: ['prettier', 'standard'],
+  extends: ['standard'],
   // required to lint *.vue files
   plugins: ['vue', 'chai-expect', 'chai-friendly'],
   // add your custom rules here
   rules: {
     'no-unused-expressions': 'off',
-    'chai-friendly/no-unused-expressions': 'error'
+    'chai-friendly/no-unused-expressions': 'error',
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ]
   },
   globals: {}
 }
