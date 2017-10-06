595d738609b01a1d869d27e488ebbfe25e947838
sunya9 <mugen.xyz@gmail.com>
2017-10-06 22:14:43 +0900

    Add tests

--- test/components/Header.js
+++ test/components/Header.js
@@ -24,8 +24,10 @@ describe('Header component', () => {
     } = shallow(Header, {
       store
     })
-    const el = vm.$el.querySelector('.nav-item.dropdown')
-    expect(el).is.null
+    vm.$nextTick(() => {
+      const el = vm.$el.querySelector('.nav-item.dropdown')
+      expect(el).is.null
+    })
   })
   it('Show user menu when logged in', done => {
     const {
@@ -41,6 +43,64 @@ describe('Header component', () => {
       const el = vm.$el.querySelector('.nav-item.dropdown')
       expect(el).is.not.null
       done()
+    })
+  })
+
+  it('Hide search form when not logged in', () => {
+    const {
+      vm
+    } = shallow(Header, {
+      store
+    })
+
+    vm.$nextTick(() => {
+      const el = vm.$el.querySelector('#search-form')
+      expect(el).is.null
+    })
+  })
+
+  it('Show search form when not logged in', () => {
+    const {
+      vm
+    } = shallow(Header, {
+      store
+    })
+    store.commit('SET_USER', {
+      username: 'foo',
+      id: 1
+    })
+    vm.$nextTick(() => {
+      const el = vm.$el.querySelector('#search-form')
+      expect(el).is.not.null
+    })
+  })
+
+  it('Hide files link when not logged in', () => {
+    const {
+      vm
+    } = shallow(Header, {
+      store
+    })
+
+    vm.$nextTick(() => {
+      const el = vm.$el.querySelector('#nav-files')
+      expect(el).is.null
+    })
+  })
+
+  it('Show files link when not logged in', () => {
+    const {
+      vm
+    } = shallow(Header, {
+      store
+    })
+    store.commit('SET_USER', {
+      username: 'foo',
+      id: 1
+    })
+    vm.$nextTick(() => {
+      const el = vm.$el.querySelector('#nav-files')
+      expect(el).is.not.null
     })
   })
 })
