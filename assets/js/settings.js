d828c961c7cdfda903d34ebc84e5673aa1a75dae
33MHz <ing@codespelunkers.com>
2017-11-05 17:20:06 -0600

    remove unused settings.js

--- assets/js/settings.js
+++
@@ -1,7 +0,0 @@
-export function isEnabledUnifiedTimeline() {
-  return window.localStorage.getItem('unified_timeline') === 'true'
-}
-
-export function isEnabledHideDirectedPosts() {
-  return window.localStorage.getItem('hide_directed_posts') === 'true'
-}
