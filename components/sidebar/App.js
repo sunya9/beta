8d25b45e7da35238930987f78fe94710421f3b78
sunya9 <mugen.xyz@gmail.com>
2018-03-15 05:47:40 +0900

    Rename .vue to .js

--- components/sidebar/App.js
+++ components/sidebar/App.js
@@ -1,4 +1,3 @@
-<script>
 import bus from '~/assets/js/bus'
 import Sidebar from './Sidebar'
 
@@ -7,61 +6,70 @@ export default {
   data() {
     const user = this.$store.state.user
     const menus = [
-      {type: 'heading', label: 'Beta', hidden: !user},
+      { type: 'heading', label: 'Beta', hidden: !user },
       {
         label: 'Your Stream',
         icon: 'fa-home',
         url: '/',
         hidden: !user
-      }, {
+      },
+      {
         label: 'Mentions',
         icon: 'fa-hand-o-right',
         url: '/mentions',
         hidden: !user
-      }, {
+      },
+      {
         label: 'Interactions',
         icon: 'fa-exchange',
         url: '/interactions',
         hidden: !user
-      }, {
+      },
+      {
         label: 'Stars',
         icon: 'fa-star',
         url: '/stars',
         hidden: !user
       },
-      {type: 'heading', label: 'Explore'},
+      { type: 'heading', label: 'Explore' },
       {
         label: 'Conversations',
         url: '/conversations',
         icon: 'fa-comments'
-      }, {
+      },
+      {
         label: 'Missed Conversations',
         url: '/missed-conversations',
         icon: 'fa-comment'
-      }, {
+      },
+      {
         label: 'Photos',
         url: '/photos',
         icon: 'fa-camera'
-      }, {
+      },
+      {
         label: 'Trending',
         url: '/trending',
         icon: 'fa-line-chart'
-      }, {
+      },
+      {
         label: 'Global',
         url: '/global',
         icon: 'fa-globe'
       },
-      {type: 'heading', label: 'Other'},
+      { type: 'heading', label: 'Other' },
       {
         label: 'About',
         url: '/about',
         icon: 'fa-info-circle'
-      }, {
+      },
+      {
         label: 'Beta on Github',
         url: 'https://github.com/sunya9/beta',
         normal: true,
         icon: 'fa-github'
-      }, {
+      },
+      {
         label: 'Keyboard shortcuts',
         click: this.showHelpModal,
         normal: true,
@@ -78,4 +86,3 @@ export default {
     }
   }
 }
-</script>
