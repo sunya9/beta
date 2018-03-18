0fba1c6708a1f4666cce589f1c1953355a817404
sunya9 <mugen.xyz@gmail.com>
2018-03-18 22:21:36 +0900

    More testable

--- components/Profile.vue
+++ components/Profile.vue
@@ -46,15 +46,15 @@
                   </small>
                 </span>
               </h3>
-              <p v-if="profile.verified" class="text-center text-md-left">
+              <p v-if="profile.verified" class="text-center text-md-left" id="profile-domain">
                 <a :href="profile.verified.link" class="u-url" rel="me">{{profile.verified.domain}}</a>
                 <i class="ml-sm-1 fa fa-check-circle-o text-success"></i>
               </p>
             </div>
           </div>
           <div v-if="user && profile.id !== user.id" class="text-center">
-            <follow-button :initial-state="profile.you_follow" :user-id="profile.id" class="mb-2" />
-            <div class="text-muted">
+            <follow-button :initial-state="profile.you_follow" :user-id="profile.id" class="mb-2" id="profile-follow-button" />
+            <div class="text-muted" id="profile-relation">
               <small>
                 {{relation}}
               </small>
@@ -66,12 +66,9 @@
     </div>
     <div class="card-body d-flex justify-content-between justify-content-md-end" id="profile-counts">
       <span class="card-link" append>{{profile.counts.posts}} Posts</span>
-      <nuxt-link v-if="user" class="card-link" to="follows" append>{{profile.counts.following}} Follows</nuxt-link>
-      <span v-if="!user" class="card-link" append>{{profile.counts.following}} Follows</span>
-      <nuxt-link v-if="user" class="card-link" to="followers" append>{{profile.counts.followers}} Followers</nuxt-link>
-      <span v-if="!user" class="card-link" append>{{profile.counts.followers}} Followers</span>
-      <nuxt-link v-if="user" class="card-link" to="starred" append>{{profile.counts.bookmarks}} Starred</nuxt-link>
-      <span v-if="!user" class="card-link" append>{{profile.counts.bookmarks}} Starred</span>
+      <nuxt-link :tag="user ? 'a' : 'span'" class="card-link" to="follows" append>{{profile.counts.following}} Follows</nuxt-link>
+      <nuxt-link :tag="user ? 'a' : 'span'" class="card-link" to="followers" append>{{profile.counts.followers}} Followers</nuxt-link>
+      <nuxt-link :tag="user ? 'a' : 'span'" class="card-link" to="starred" append>{{profile.counts.bookmarks}} Starred</nuxt-link>
       <a class="card-link" :href="'https://api.pnut.io/v0/feed/rss/users/' + profile.id + '/posts'"><i class="fa fa-rss-square" aria-hidden="true"></i> RSS</a>
     </div>
   </div>
