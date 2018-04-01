ba0933f0b830c2aeb0c85a3735ca9d123d050eb5
sunya9 <mugen.xyz@gmail.com>
2018-04-02 01:43:55 +0900

    Improve tests

--- test/components/Compose.js
+++ test/components/Compose.js
@@ -203,7 +203,6 @@ describe('Compose component', () => {
           expect(res).to.be.an.instanceof(Promise)
           await res
           expect(vm.promise).to.be.null
-          expect(vm.error).to.be.null
           expect(vm.rawText).to.be.a('string').that.is.empty
           expect(vm.photos).to.be.an('array').that.is.empty
         })
@@ -221,4 +220,15 @@ describe('Compose component', () => {
       })
     })
   })
+  // context('when unpaid users', () => {
+  //   it('add photo is hidden', () => {
+  //     console.log(vm.$el)
+  //     expect(vm.$el.querySelector('.add-photo')).is.null
+  //   })
+  // })
+  // context('when paid users', () => {
+  //   it('add photo is visible', () => {
+  //     expect(vm.$el.querySelector('.add-photo')).is.not.null
+  //   })
+  // })
 })
--- test/fixtures/axios-mock.js
+++ test/fixtures/axios-mock.js
@@ -3,7 +3,7 @@ const MockAdapter = require('axios-mock-adapter')
 
 const mock = new MockAdapter(axios)
 
-mock.onAny(/\/proxy\/users\/\d+\//).reply(200)
+mock.onAny(/\/users\/\d+\//).reply(200)
 mock.onAny(/\/posts/).reply(200)
 mock.onAny(/\/test|\/proxy\/test/).reply(200)
 mock.onPost(/\/proxy\/users\/me\/avatar/).reply(200, {
