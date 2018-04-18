214ec4f6cc6a95291166c75069e82dddc05563bd
sunya9 <mugen.xyz@gmail.com>
2018-04-18 23:06:53 +0900

    Add your polls page

---
+++ pages/polls.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <list :data="data" type="Poll" :option="option" ref="list" />
+  </div>
+</template>
+<script>
+import Compose from '~/components/Compose'
+import List from '~/components/List'
+
+export default {
+  async asyncData({ app: { $resource } }) {
+    const option = {}
+    const data = await $resource(option)
+    return { data, option }
+  },
+  components: {
+    List,
+    Compose
+  },
+  head() {
+    return {
+      title: 'Polls'
+    }
+  }
+}
+</script>
