17f8e447328b3e613fa1ac369c34ce5f2ff4edb8
sunya9 <mugen.xyz@gmail.com>
2018-05-31 05:55:00 +0900

    Fix tests

--- test/components/Avatar.js
+++ test/components/Avatar.js
@@ -27,28 +27,28 @@ describe('Avatar component', () => {
     localStorage.setItem('square_avatars', 'true')
     const wrapper = mount(Avatar, opts)
     await wrapper.vm.$nextTick()
-    expect(wrapper.first('img').hasClass('rounded-circle')).to.be.false
+    expect(wrapper.find('img').classes()).to.not.contain('rounded-circle')
   })
   it('be rounded icon when square_avatars not equals `true`', () => {
     expect(
       mount(Avatar, opts)
-        .first('img')
-        .hasClass('rounded-circle')
-    ).to.be.true
+        .find('img')
+        .classes()
+    ).to.contain('rounded-circle')
   })
   it('Add w param when max-size is larger than 0', () => {
     opts.propsData.maxSize = 10
     const wrapper = mount(Avatar, opts)
-    expect(wrapper.first('img').getAttribute('src')).to.have.string('?w=10')
+    expect(wrapper.find('img').attributes().src).to.have.string('?w=10')
   })
   it('size attr equals width and height', () => {
     opts.propsData.size = 32
-    let img = mount(Avatar, opts).first('img')
-    expect(img.getAttribute('width')).to.equal('32')
-    expect(img.getAttribute('height')).to.equal('32')
+    let img = mount(Avatar, opts).find('img')
+    expect(img.attributes().width).to.equal('32')
+    expect(img.attributes().height).to.equal('32')
     opts.propsData.size = 64
-    img = mount(Avatar, opts).first('img')
-    expect(img.getAttribute('width')).to.equal('64')
-    expect(img.getAttribute('height')).to.equal('64')
+    img = mount(Avatar, opts).find('img')
+    expect(img.attributes().width).to.equal('64')
+    expect(img.attributes().height).to.equal('64')
   })
 })
--- test/fixtures/axios-mock.js
+++ test/fixtures/axios-mock.js
@@ -3,7 +3,19 @@ const MockAdapter = require('axios-mock-adapter')
 
 const mock = new MockAdapter(axios)
 
-mock.onAny(/\/users\/\d+\//).reply(200)
+mock.onPut(/\/users\/\d+\/follow/).reply(200, {
+  data: {
+    you_follow: true
+  }
+})
+mock.onDelete(/\/users\/\d+\/follow/).reply(200, {
+  data: {
+    you_follow: false
+  }
+})
+mock.onAny(/\/users\/\d+\//).reply(200, {
+  data: {}
+})
 mock.onAny(/\/posts/).reply(200)
 mock.onAny(/\/test|\/proxy\/test/).reply(200)
 mock.onPost(/\/proxy\/users\/me\/avatar/).reply(200, {
