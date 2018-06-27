aaef20df50ec004b21b8f6b7b2c97fe176e635c2
33MHz <ing@codespelunkers.com>
2018-06-27 07:36:54 -0500

    fix #114

--- components/Channel.vue
+++ components/Channel.vue
@@ -1,33 +1,35 @@
 <template>
-	<nuxt-link :to="`/messages/${channel.id}`">
-		<div class="media">
-			<avatar :avatar="{ link: opponent.avatar_image }" size="32" class="mr-2" />
-			<div class="media-body" style="overflow: hidden">
-				<h5>
-					<span v-if="opponent.name">
-						{{opponent.name}}
-						<small class="text-muted">
-							@{{opponent.username}}
-						</small>
-					</span>
-					<span v-else>
-						@{{opponent.username}}
-					</span>
-					<i v-show="channel.you_muted" class="fa fa-bell-slash-o"></i>
-				</h5>
-				<p v-if="channel.recent_message" class="mb-0 text-truncate">
-					@{{channel.recent_message.user.username}}:
-					<span v-if="!channel.recent_message.is_deleted">
-						{{channel.recent_message.content.text}}
-					</span>
-					<span v-else class="text-muted">[Post deleted]</span>
-				</p>
-			</div>
-			<span class="align-self-center">
-				<i class="fa fa-chevron-right"></i>
-			</span>
-		</div>
-	</nuxt-link>
+  <nuxt-link :to="`/messages/${channel.id}`">
+    <div class="media">
+      <avatar :avatar="{ link: opponent.avatar_image }" size="32" class="mr-2" />
+      <div class="media-body" style="overflow: hidden">
+        <h5>
+          <i v-show="channel.you_muted" class="fa fa-bell-slash-o" style="float:right"></i>
+          <span v-if="opponent.name">
+            {{opponent.name}}
+            <small class="text-muted">
+              @{{opponent.username}}<span v-if="subscribers.length">, </span>
+            </small>
+          </span>
+          <span v-else>
+            @{{opponent.username}}<small v-if="subscribers.length" class="text-muted">, </small>
+          </span>
+          <span v-if="subscribers.length" v-for="user in subscribers" :key="user.id" class="text-muted">
+            <small><span v-if="user.id !== subscribers[0].id">, </span>@{{user.username}}</small>
+          </span>
+        </h5>
+        <p v-if="channel.recent_message" class="mb-0 text-truncate">
+          <span v-if="!channel.recent_message.is_deleted">
+            {{channel.recent_message.content.text}}
+          </span>
+          <span v-else class="text-muted">[Post deleted]</span>
+        </p>
+      </div>
+      <span class="align-self-center">
+        <i class="fa fa-chevron-right"></i>
+      </span>
+    </div>
+  </nuxt-link>
 </template>
 
 <script>
@@ -41,9 +43,7 @@ export default {
   computed: {
     ...mapState(['user']),
     opponent() {
-      if (this.channel.owner.id === this.user.id) {
-        return this.channel.acl.write.user_ids[0]
-      } else {
+      if (this.channel.recent_message && this.channel.recent_message.user) {
         const {
           username,
           name,
@@ -51,8 +51,7 @@ export default {
           content: {
             avatar_image: { link: avatar_image }
           }
-        } = this.channel.owner
-
+        } = this.channel.recent_message.user
         const res = {
           username,
           name,
@@ -60,7 +59,57 @@ export default {
           avatar_image
         }
         return res
+      } else if (this.channel.owner.id === this.user.id) {
+        return this.channel.acl.write.user_ids[0]
       }
+
+      const {
+        username,
+        name,
+        id,
+        content: {
+          avatar_image: { link: avatar_image }
+        }
+      } = this.channel.owner
+
+      const res = {
+        username,
+        name,
+        id,
+        avatar_image
+      }
+      return res
+    },
+    subscribers() {
+      // don't include self or most recent messager
+      const {
+        username,
+        name,
+        id,
+        content: {
+          avatar_image: { link: avatar_image }
+        }
+      } = this.channel.owner
+      const owner = {
+        username,
+        name,
+        id,
+        avatar_image
+      }
+
+      const subscribers = this.channel.acl.write.user_ids.filter(user => {
+        return (
+          user.id !== this.user.id &&
+          (!this.channel.recent_message ||
+            user.id !== this.channel.recent_message.user.id)
+        )
+      })
+
+      if (this.channel.owner.id !== this.user.id) {
+        subscribers.push(owner)
+      }
+
+      return subscribers
     }
   },
   components: {
