49e78098668348bbfb0455e3ece281477071a2c9
sunya9 <mugen.xyz@gmail.com>
2018-08-25 06:12:37 +0900

    Improve messages layout

---
+++ pages/messages.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-md-8">
+        <ul class="nav nav-tabs mb-4">
+          <li class="nav-item">
+            <nuxt-link to="/messages" class="nav-link" exact :class="{ active: isPM === true }">
+              Messages
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/messages?public" class="nav-link" :class="{ active: isPM === false }">
+              Public Rooms
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <nuxt-child @updateNav="isPM = $event" />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isPM: null
+    }
+  },
+  watch: {
+    '$route.fullPath'() {
+      this.isPM = null
+    }
+  }
+}
+</script>
