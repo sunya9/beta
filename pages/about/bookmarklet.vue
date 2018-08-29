7f7c4eb1dfbeb492ba78daf8b893f5b0f676dd22
33MHz <ing@codespelunkers.com>
2018-08-29 08:41:31 -0500

    update bookmarklet URL

--- pages/about/bookmarklet.vue
+++ pages/about/bookmarklet.vue
@@ -9,7 +9,7 @@
 
 <script>
 const shareScript = `
-javascript:(function(){var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,W=window;if(C>A){G=Math.round((C/2)-(A/2))}W.open('https://beta.unsweets.net/intent/post?text='+encodeURIComponent(document.title)+'&url='+encodeURIComponent(W.location),'','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');}());
+javascript:(function(){var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,W=window;if(C>A){G=Math.round((C/2)-(A/2))}W.open('https://beta.pnut.io/intent/post?text='+encodeURIComponent(document.title)+'&url='+encodeURIComponent(W.location),'','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');}());
 `
 
 export default {
