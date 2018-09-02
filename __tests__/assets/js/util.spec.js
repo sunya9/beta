5a1dc13f9148c34854710e3318f5abf4b21f4b1f
sunya9 <mugen.xyz@gmail.com>
2018-09-02 10:48:03 +0900

    Add getRSSLink test

---
+++ __tests__/assets/js/util.spec.js
@@ -0,0 +1,12 @@
+import * as util from '~/assets/js/util'
+
+describe('util', () => {
+  test('getRSSLink', () => {
+    expect(util.getRSSLink('foo')).toEqual({
+      hid: 'rss',
+      rel: 'alternate',
+      type: 'application/rss+xml',
+      href: 'foo'
+    })
+  })
+})
