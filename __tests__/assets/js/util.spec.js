9fd9751f875e2800672c12e66889234375deeb51
sunya9 <mugen.xyz@gmail.com>
2018-09-02 19:58:00 +0900

    Add the getImageURLs test case

--- __tests__/assets/js/util.spec.js
+++ __tests__/assets/js/util.spec.js
@@ -9,4 +9,81 @@ describe('util', () => {
       href: 'foo'
     })
   })
+  describe('getImageURLs', () => {
+    it('get from link', () => {
+      expect(
+        util.getImageURLs({
+          content: {
+            entities: {
+              links: [
+                {
+                  link: 'photo.jpeg'
+                }
+              ]
+            }
+          }
+        })
+      ).toEqual([
+        {
+          thumb: 'photo.jpeg',
+          original: 'photo.jpeg'
+        }
+      ])
+    })
+    it('get from raw', () => {
+      expect(
+        util.getImageURLs(
+          {
+            content: {},
+            raw: [
+              {
+                type: 'io.pnut.core.oembed',
+                value: {
+                  type: 'photo',
+                  url: 'original.png',
+                  thumbnail_url: 'thumbnail.png'
+                }
+              }
+            ]
+          },
+          true
+        )
+      ).toMatchObject([
+        {
+          thumb: 'thumbnail.png',
+          original: 'original.png'
+        }
+      ])
+    })
+    it('Remove duplicate', () => {
+      expect(
+        util.getImageURLs({
+          content: {
+            entities: {
+              links: [
+                {
+                  link: 'original.png'
+                }
+              ]
+            }
+          },
+          raw: [
+            {
+              type: 'io.pnut.core.oembed',
+              value: {
+                type: 'photo',
+                url: 'original.png',
+                thumbnail_url: 'thumbnail.png'
+              }
+            }
+          ]
+        })
+      ).toMatchObject([
+        {
+          thumb: 'thumbnail.png',
+          original: 'original.png'
+        }
+      ])
+    })
+  })
 })
