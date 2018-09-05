ec59ab5d0f164983965679352215491b6911c7c8
sunya9 <mugen.xyz@gmail.com>
2018-09-06 06:30:18 +0900

    Create modal plugin

---
+++ plugins/modal/mediator.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+
+export default new Vue({
+  data() {
+    return {
+      modals: {}
+    }
+  },
+  methods: {
+    async show(id, arg) {
+      if (!this.modals[id]) return
+      return this.modals[id].show(arg)
+    },
+    async ok(id, arg) {
+      if (!this.modals[id]) return
+      return this.modals[id].ok(arg)
+    },
+    async cancel(id, arg) {
+      if (!this.modals[id]) return
+      return this.modals[id].cancel(arg)
+    }
+  }
+})
