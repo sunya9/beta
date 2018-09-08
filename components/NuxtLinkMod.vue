9cafafb8990cb73d116c7152ed438b2ba24adf37
sunya9 <mugen.xyz@gmail.com>
2018-09-09 03:52:29 +0900

    Add NuxtLinkMod component

---
+++ components/NuxtLinkMod.vue
@@ -0,0 +1,35 @@
+<template>
+  <nuxt-link
+    v-if="ownDomain"
+    v-bind="$attrs"
+    :to="href"
+  >
+    <slot/>
+  </nuxt-link>
+  <a
+    v-else
+    v-bind="$attrs"
+    :href="href"
+  >
+    <slot />
+  </a>
+</template>
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      default: '',
+      required: true
+    }
+  },
+  computed: {
+    ownDomain() {
+      return this.to.startsWith('https://beta.pnut.io')
+    },
+    href() {
+      return this.to.replace(/^https:\/\/beta\.pnut\.io/, '')
+    }
+  }
+}
+</script>
