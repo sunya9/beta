2ceeb69e4a5563abc8ae25b9aae01cbf69cdc477
sunya9 <mugen.xyz@gmail.com>
2018-09-10 06:25:36 +0900

    Improve channel modal code

--- components/ChannelMemberEditModal.vue
+++ components/ChannelMemberEditModal.vue
@@ -7,26 +7,28 @@
     auto-focus="cancel"
     suppress-warnings
     @show="show"
+    @shown="shown"
+    @hidden="hidden"
   >
     <div
-      v-if="vm"
+      v-if="acl"
       class="form-group"
     >
       <div class="form-group">
         <h5>Access</h5>
         <custom-checkbox
           v-model="anyUserWrite"
-          :disabled="vm.channel.acl.write.immutable">
+          :disabled="acl.write.immutable">
           Any user can write
         </custom-checkbox>
         <custom-checkbox
           v-model="anyUserRead"
-          :disabled="vm.channel.acl.read.immutable || anyUserWrite || publicRead">
+          :disabled="acl.read.immutable || anyUserWrite || publicRead">
           Any user can read
         </custom-checkbox>
         <custom-checkbox
           v-model="publicRead"
-          :disabled="vm.channel.acl.read.immutable">
+          :disabled="acl.read.immutable">
           Publicly readable
         </custom-checkbox>
       </div>
@@ -34,51 +36,83 @@
         <div class="row flex-">
           <div class="col-sm col-md-12">
             <h5>Members</h5>
-            <div
-              v-if="!anyUserWrite || is_owner"
-              class="form-group">
-              <button
-                :disabled="disabledAdd"
-                class="btn btn-link"
-                type="button"
-                @click="addUser"
-              >
-                <font-awesome-icon
-                  icon="plus"
-                  class="mr-1"
-                />
-                <span>Add</span>
-              </button>
+            <div class="form-group">
+              <div
+                v-if="!anyUserWrite || isOwner"
+                class="input-group">
+                <input
+                  ref="addUserForm"
+                  :disabled="disabledAdd"
+                  v-model="newUser.username"
+                  type="text"
+                  placeholder="Username"
+                  class="form-control"
+                  pattern="(\w+){0,20}"
+                  title="Username"
+                  @keydown.enter="addUser"
+                >
+                <div class="input-group-append">
+                  <acl-select
+                    v-model="newUser.acl"
+                    :any-user-read="anyUserRead"
+                    :any-user-write="anyUserWrite"
+                    :owner-id="owner.id"
+                  />
+                  <button
+                    :disabled="disabledAddButton"
+                    class="btn btn-link"
+                    type="button"
+                    @click="addUser"
+                  >
+                    <font-awesome-icon
+                      icon="plus"
+                      class="mr-1"
+                    />
+                    <span>Add</span>
+                  </button>
+                </div>
+              </div>
             </div>
             <ul class="list-unstyled">
               <li
                 v-for="(user, index) in users"
-                :key="index"
-                class="mb-2">
-                <input
-                  :disabled="vm.channel.acl[user.acl].immutable || (!is_owner && users[index].acl == 'full')"
-                  v-model="users[index].username"
-                  type="text"
-                  placeholder="Username"
-                  pattern="(\w+){0,20}"
-                  title="Username"
-                >
-                <select
-                  :disabled="vm.channel.acl[user.acl].immutable || (!is_owner && users[index].acl == 'full')"
-                  v-model="users[index].acl">
-                  <option v-if="!anyUserRead">read</option>
-                  <option v-if="!anyUserWrite">write</option>
-                  <option
-                    v-if="users[index].acl == 'full' || is_owner"
-                    value="full">moderate</option>
-                </select>
-                <button
-                  v-if="!vm.channel.acl[user.acl].immutable && (is_owner || users[index].acl != 'full')"
-                  type="button"
-                  class="btn btn-link"
-                  @click="removeUser(index)">
-                  <font-awesome-icon icon="times" />
-                </button>
+                :key="user.username">
+                <div class="form-group">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <div class="input-group-text">
+                        <avatar
+                          :avatar="`@${user.username}`"
+                          size="16"
+                          max-size="16"
+                        />
+                      </div>
+                    </div>
+                    <input
+                      :value="user.username"
+                      type="text"
+                      class="form-control"
+                      readonly
+                    >
+                    <div class="input-group-append">
+                      <acl-select
+                        v-model="user.acl"
+                        :any-user-read="anyUserRead"
+                        :any-user-write="anyUserWrite"
+                        :disabled="editDisabled(user)"
+                        :your-acl="user.acl"
+                        :owner-id="owner.id"
+                      />
+                      <button
+                        :disabled="editDisabled(user)"
+                        type="button"
+                        class="btn btn-link"
+                        @click="removeUser(index)">
+                        <font-awesome-icon icon="times" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </li>
             </ul>
           </div>
@@ -92,39 +126,52 @@
 import { mapGetters } from 'vuex'
 import CustomCheckbox from '~/components/CustomCheckbox'
 import BaseModal from '~/components/BaseModal'
+import Avatar from '~/components/Avatar'
+import AclSelect from '~/components/AclSelect'
+import { cloneDeep, groupBy } from 'lodash'
 
 export default {
   components: {
+    AclSelect,
+    Avatar,
     BaseModal,
     CustomCheckbox
   },
   data() {
     return {
-      vm: null,
+      owner: null,
       users: [],
       acl: null,
       anyUserWrite: null,
       anyUserRead: null,
-      publicRead: null
+      publicRead: null,
+      newUser: {
+        username: '',
+        acl: this.minimumPermission
+      }
     }
   },
   computed: {
     ...mapGetters(['user']),
-    chat() {
-      return this.vm.channel.raw.find(r => {
-        return r.type === 'io.pnut.core.chat-settings'
-      }).value
-    },
-    is_owner() {
-      return this.user && this.user.id === this.vm.channel.owner.id
+    isOwner() {
+      return this.user && this.user.id === this.owner.id
     },
     disabledAdd() {
+      return this.users.length + 1 >= 200
+    },
+    disabledAddButton() {
       return (
-        this.users.length + 1 >= 200 ||
-        this.users.find(user => {
-          return user.username == ''
-        })
+        this.disabledAdd ||
+        !this.newUser.username ||
+        this.users.find(user => user.username === this.newUser.username) ||
+        this.user.username === this.newUser.username ||
+        this.owner.username === this.newUser.username
       )
+    },
+    minimumPermission() {
+      if (this.anyUserRead) return 'write'
+      if (this.anyUserWrite) return 'full'
+      return 'read'
     }
   },
   watch: {
@@ -151,93 +198,80 @@ export default {
     }
   },
   methods: {
-    show(vm) {
-      this.vm = vm
+    editDisabled(user) {
+      const userIsModerator = !this.isOwner && user.acl === 'full'
+      return this.acl[user.acl].immutable || userIsModerator
+    },
+    shown() {
+      this.$refs.addUserForm.focus()
+    },
+    show(channel) {
+      this.newUser = {
+        username: '',
+        acl: this.minimumPermission
+      }
+      this.owner = channel.owner
+      this.acl = cloneDeep(channel.acl)
       this.users.push(
-        ...this.vm.channel.acl.full.user_ids.map(user => {
-          return { username: user.username, acl: 'full' }
-        })
+        ...this.acl.full.user_ids.map(user => ({
+          username: user.username,
+          acl: 'full'
+        }))
       )
       this.users.push(
-        ...this.vm.channel.acl.write.user_ids.map(user => {
+        ...this.acl.write.user_ids.map(user => {
           return { username: user.username, acl: 'write' }
         })
       )
       this.users.push(
-        ...this.vm.channel.acl.read.user_ids.map(user => {
+        ...this.acl.read.user_ids.map(user => {
           return { username: user.username, acl: 'read' }
         })
       )
-      this.acl = JSON.parse(JSON.stringify(this.vm.channel.acl))
       this.anyUserWrite = this.acl.write.any_user
       this.anyUserRead = this.acl.read.any_user
       this.publicRead = this.acl.read.public
     },
     ok() {
-      const updatedChannel = JSON.parse(JSON.stringify(this.vm.channel))
-
-      this.acl.read.user_ids = this.users
-        .filter(u => {
-          return u.username != '' && u.acl == 'read'
-        })
-        .map(u => {
-          return '@' + u.username.replace(/[^\w]/g, '')
-        })
-      this.acl.write.user_ids = this.users
-        .filter(u => {
-          return u.username != '' && u.acl == 'write'
-        })
-        .map(u => {
-          return '@' + u.username.replace(/[^\w]/g, '')
-        })
-      this.acl.full.user_ids = this.users
-        .filter(u => {
-          return u.username != '' && u.acl == 'full'
-        })
-        .map(u => {
-          return '@' + u.username.replace(/[^\w]/g, '')
-        })
-
-      if (this.vm.channel.acl.read.immutable) {
-        delete this.acl.read
-      }
-      if (this.vm.channel.acl.write.immutable) {
-        delete this.acl.write
-      }
-      if (this.vm.channel.acl.full.immutable || !this.is_owner) {
-        delete this.acl.full
-      }
-      updatedChannel.acl = this.acl
-
-      this.vm.update(updatedChannel)
+      const usersMap = groupBy(
+        this.users.filter(user => user.username),
+        user => user.acl
+      )
+      Object.keys(usersMap).forEach(
+        permission =>
+          (this.acl[permission].user_ids = usersMap[permission].map(atUserStr))
+      )
+      if (this.acl.read.immutable) delete this.acl.read
+      if (this.acl.write.immutable) delete this.acl.write
+      if (this.acl.full.immutable || !this.isOwner) delete this.acl.full
+      return this.acl
     },
     hidden() {
-      this.vm = null
       this.users = []
       this.acl = null
     },
     addUser() {
-      if (this.anyUserWrite) {
-        this.users.unshift({ username: '', acl: 'full' })
-      } else if (this.anyUserRead) {
-        this.users.unshift({ username: '', acl: 'write' })
-      } else {
-        this.users.unshift({ username: '', acl: 'read' })
-      }
+      if (this.disabledAdd || this.disabledAddButton) return
+      this.users.unshift({ ...this.newUser })
+      this.newUser.username = ''
     },
     removeUser(i) {
       this.users.splice(i, 1)
     },
     verifyACL() {
       this.users = this.users.map(user => {
-        if (this.anyUserRead && user.acl == 'read') {
+        if (this.anyUserRead && user.acl === 'read') {
           user.acl = 'write'
-        } else if (this.anyUserWrite && user.acl == 'write') {
+        } else if (this.anyUserWrite && user.acl === 'write') {
           user.acl = 'full'
         }
         return user
       })
     }
   }
+}
+
+function atUserStr(user) {
+  return `@${user.username.replace(/[^\w]/g, '')}`
 }
 </script>
