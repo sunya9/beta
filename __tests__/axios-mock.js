5bb8e3e4abdb3d922b3a519c5f971a69e6db47a3
sunya9 <mugen.xyz@gmail.com>
2018-09-14 01:37:03 +0900

    Add mock resource

--- __tests__/axios-mock.js
+++ __tests__/axios-mock.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
-
+import fixtures from './fixtures'
 const mock = new MockAdapter(axios)
 
 mock.onAny(/\/200/).reply(200, {
@@ -24,14 +24,10 @@ mock.onAny(/\/users\/\d+\//).reply(200, {
   data: {}
 })
 mock.onAny(/\/posts/).reply(200)
-mock.onAny(/\/test|\/proxy\/test/).reply(200)
-mock.onPost(/\/proxy\/users\/me\/avatar/).reply(200, {
-  meta: [],
-  data: {
-    content: {
-      avatar_image: {}
-    }
-  }
+mock.onAny(/\/test/).reply(200)
+mock.onPost(/\/users\/me\/avatar/).reply(200, {
+  meta: {},
+  data: fixtures('profile', 'newAvatar')
 })
 
 export default mock
