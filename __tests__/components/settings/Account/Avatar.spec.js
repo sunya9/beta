b8ee5228b1b6ae74a6329434f9684af373eaec61
sunya9 <mugen.xyz@gmail.com>
2018-09-14 01:35:40 +0900

    Move fixtures

--- __tests__/components/settings/Account/Avatar.spec.js
+++ __tests__/components/settings/Account/Avatar.spec.js
@@ -1,88 +1,65 @@
 import Avatar from '~/components/settings/Account/Avatar'
-import { shallowMount } from 'helper'
+import { shallowMount, fixtures } from 'helper'
 
 describe('Avatar component', () => {
-  let wrapper, $input, $button, $toast
-  const newAvatar = {
-    is_default: false,
-    width: 100,
-    height: 100,
-    link: 'http://example.com/new.png'
-  }
-  const oldAvatar = {
-    link: 'http://example.com/old.png',
-    width: 100,
-    height: 200,
-    is_default: false
-  }
+  let wrapper, $input, $toast, $image
+  const oldAvatar = fixtures('profile').content.avatar_image
   beforeEach(() => {
     wrapper = shallowMount(Avatar, {
       propsData: {
         avatar: oldAvatar
       }
     })
-    $input = wrapper.vm.$el.querySelector('input[type="file"]')
-    $button = wrapper.vm.$el.querySelector('button')
     $toast = wrapper.vm.$toast
+    $input = wrapper.find('input[type="file"]')
+    $image = wrapper.find('img')
   })
-  test('show file picker when clicked button', async () => {
-    const picker = jest.fn()
-    $input.onclick = picker
-    $button.click()
-    await wrapper.vm.$nextTick()
-    expect(picker).toHaveBeenCalled()
+  test('show file picker when button is clicked', async () => {
+    const handler = jest.fn()
+    $input.element.onclick = handler
+    wrapper.find('button').trigger('click')
+    expect(handler).toHaveBeenCalled()
   })
-  test('return false when not be picked', async () => {
-    expect(
-      await wrapper.vm.avatarChanged({
-        target: {
-          files: []
-        }
-      })
-    ).toBe(false)
+  test('Avatar does not be changed when not be picked', () => {
+    const handler = jest.fn()
+    $input.element.onchange = handler
+    $input.trigger('change')
+    expect(handler).toHaveBeenCalled()
+    expect($image.attributes().src).toBe(oldAvatar.link)
   })
   test('show error toast when file size over 2MiB', async () => {
-    const file = {
-      size: 2097001
-    }
     await wrapper.vm.avatarChanged({
       target: {
-        files: [file]
+        files: [
+          {
+            size: 2097001
+          }
+        ]
       }
     })
     expect(wrapper.vm.$toast.error).toHaveBeenCalled()
   })
   test('show toast and update img when succeed at uploading avatar', async () => {
-    const file = new File([], 'foo.png')
-    wrapper.vm.$axios = {
-      $post: jest.fn(() =>
-        Promise.resolve({
-          data: {
-            content: {
-              avatar_image: newAvatar
-            }
-          }
-        })
-      )
-    }
     await wrapper.vm.avatarChanged({
       target: {
-        files: [file]
+        files: [
+          new File(
+            [],
+            fixtures('profile', 'newAvatar').content.avatar_image.link
+          )
+        ]
       }
     })
     expect($toast.success).toHaveBeenCalled()
     expect($toast.error).not.toHaveBeenCalled()
-    await wrapper.vm.$nextTick()
-    expect(wrapper.vm.$el.querySelector('img').src).toBe(newAvatar.link)
+    expect(wrapper.vm.$el.querySelector('img').src).not.toBe(oldAvatar.link)
   })
   test('throw error when failed to request', async () => {
     const file = new Blob([])
     const $post = jest.fn(() => {
       throw new Error()
     })
-    wrapper.vm.$axios = {
-      $post
-    }
+    wrapper.vm.$axios.$post = $post
     await wrapper.vm.avatarChanged({
       target: {
         files: [file]
