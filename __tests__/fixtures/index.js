6aee2f953b3c4c0bd5f2f542f3b4fbda7b605907
sunya9 <mugen.xyz@gmail.com>
2018-09-16 06:57:34 +0900

    Improve arguments

--- __tests__/fixtures/index.js
+++ __tests__/fixtures/index.js
@@ -1,9 +1,12 @@
 import { merge, cloneDeep } from 'lodash'
 import path from 'path'
 
-export default function fixtures(filepath, overrides) {
+export default function fixtures(filepath, ...overrides) {
   const defExport = require(path.resolve(__dirname, filepath))
   return overrides
-    ? merge({}, defExport.default, defExport[overrides])
+    ? overrides.reduce(
+        (res, key) => merge({}, res, defExport[key]),
+        defExport.default
+      )
     : cloneDeep(defExport.default)
 }
