748944b47ec4219777d507fe2515bf27df88f452
sunya9 <mugen.xyz@gmail.com>
2018-09-16 13:33:00 +0900

    Add mocks

--- __tests__/fixtures/channel.js
+++ __tests__/fixtures/channel.js
@@ -1,3 +1,19 @@
 export default {
   id: '1'
 }
+
+export const publicly = {
+  acl: {
+    read: {
+      public: true
+    }
+  }
+}
+
+export const writable = {
+  acl: {
+    write: {
+      you: true
+    }
+  }
+}
