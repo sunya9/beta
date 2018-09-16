a68a2ae4f1548652991c61d8add7863c54659902
sunya9 <mugen.xyz@gmail.com>
2018-09-17 08:40:58 +0900

    Create single page of polls

---
+++ pages/polls/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <list
+    :data="data"
+    :auto-refresh="false"
+    :options="options"
+    type="Poll" />
+</template>
+<script>
+import List from '~/components/List'
+
+export default {
+  components: {
+    List
+  },
+  async asyncData({ app: { $resource } }) {
+    const options = {}
+    const data = await $resource()
+    return {
+      data: {
+        ...data,
+        data: [data.data]
+      },
+      options
+    }
+  }
+}
+</script>
