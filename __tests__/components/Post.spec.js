f8db44a85fb4477a54ed28c784f8a224bd8794a3
sunya9 <mugen.xyz@gmail.com>
2018-11-09 02:42:20 +0900

    Move and fix tests

--- __tests__/components/Post.spec.js
+++ __tests__/components/Post.spec.js
@@ -1,16 +1,11 @@
 import Post from '~/components/Post'
 import EntityText from '~/components/EntityText'
-import {
-  shallowMount,
-  authedUserCreateStore,
-  baseMountOpts,
-  fixtures
-} from 'helper'
+import { mount, authedUserCreateStore, baseMountOpts, fixtures } from 'helper'
 
 describe('Post component', () => {
   let wrapper
   beforeEach(() => {
-    wrapper = shallowMount(
+    wrapper = mount(
       Post,
       baseMountOpts({
         stubs: {
@@ -32,6 +27,27 @@ describe('Post component', () => {
       })
       await wrapper.vm.$nextTick()
       expect(wrapper.text()).toContain('[Post deleted]')
+    })
+  })
+  describe('has long post', () => {
+    beforeEach(() => {
+      wrapper.setProps({
+        post: fixtures('post', 'hasLongpost')
+      })
+    })
+    it('Show Expand/Collapse button', () => {
+      const collapseText = 'Collapse Post'
+      const expandText = 'Expand Post'
+      expect(wrapper.text()).toContain(expandText)
+      expect(wrapper.text()).not.toContain(collapseText)
+      wrapper.find('button[data-test-collapse-button]').trigger('click')
+      expect(wrapper.text()).not.toContain(expandText)
+      expect(wrapper.text()).toContain(collapseText)
+    })
+    it('Show longpost content', () => {
+      wrapper.find('button[data-test-collapse-button]').trigger('click')
+      expect(wrapper.text()).toContain('title')
+      expect(wrapper.text()).toContain('body')
     })
   })
 })
