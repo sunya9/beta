cb836e13c151ff0efb94ba0f44452eb9e91ade3b
sunya9 <mugen.xyz@gmail.com>
2018-11-09 02:04:47 +0900

    Move long post and spoiler

--- components/EntityText.vue
+++ components/EntityText.vue
@@ -1,102 +1,58 @@
 <template>
-  <span
-    v-if="hasEntities && !deleted">
-    <template v-if="spoiler && !showSpoiler">
-      <button
-        class="btn btn-link mr-3 btn-primary"
-        type="button"
-        @click="toggleSpoiler">
-        <span class="d-sm-inline ml-2">Show Spoiler:
-          <emojify :text="spoiler.topic" />
-        </span>
-      </button>
-    </template>
-    <template v-else-if="!longpost || !showLongpost">
-      <span class="apply-pre">
-        <template v-for="(entity, i) in entities">
-          <nuxt-link
-            v-if="entity.type === 'mentions'"
-            :to="`/@${entity.text}`"
-            :key="`mention-${i}`"
-          >@{{ entity.text }}</nuxt-link>
+  <span v-if="hasEntities && !deleted">
+    <span class="apply-pre">
+      <template v-for="(entity, i) in entities">
+        <nuxt-link
+          v-if="entity.type === 'mentions'"
+          :to="`/@${entity.text}`"
+          :key="`mention-${i}`"
+        >@{{ entity.text }}</nuxt-link
+        >
+        <emojify
+          v-else-if="entity.type === 'tags'"
+          :to="`/tags/${entity.text}`"
+          :text="`#${entity.text}`"
+          :key="`tags-${i}`"
+          element="nuxt-link"
+        />
+        <template v-else-if="entity.type === 'links'">
           <emojify
-            v-else-if="entity.type === 'tags'"
-            :to="`/tags/${entity.text}`"
-            :text="`#${entity.text}`"
-            :key="`tags-${i}`"
-            element="nuxt-link"
+            :to="entity.replace ? entity.replace.link : entity.link"
+            :key="`links-${i}`"
+            :element="$options.components.NuxtLinkMod"
+            :text="replaceLinkText(entity)"
+            target="_new"
           />
-          <template v-else-if="entity.type === 'links'">
-            <emojify
-              :to="entity.replace ? entity.replace.link : entity.link"
-              :key="`links-${i}`"
-              :element="$options.components.NuxtLinkMod"
-              :text="replaceLinkText(entity)"
-              target="_new"
-            />
-            <span
-              v-if="entity.amended_len"
-              :key="`links-${i}-domain`"
-            >{{ entity.replace ? ` [${entity.replace.domain}]` : unicodeSubstring(entity.text, entity.len, entity.amended_len) }}</span>
-            <a
-              v-if="entity.replace"
-              :key="`links-${i}-replaced-icon`"
-              :data-content="`<a href='${entity.link}' target='_new'>${entity.link}</a>`"
-              data-toggle="popover"
-              data-trigger="click"
-              href="#"
-              class="mx-1"
-              @click.prevent
-            >
-              <font-awesome-icon
-                icon="info-circle"
-              />
-            </a>
-          </template>
-          <emojify
-            v-else
-            :text="entity.text"
-            :key="`text-${i}`" />
+          <span
+            v-if="entity.amended_len"
+            :key="`links-${i}-domain`">{{
+              entity.replace
+                ? ` [${entity.replace.domain}]`
+                : unicodeSubstring(entity.text, entity.len, entity.amended_len)
+            }}</span>
+          <a
+            v-if="entity.replace"
+            :key="`links-${i}-replaced-icon`"
+            :data-content="
+              `<a href='${entity.link}' target='_new'>${entity.link}</a>`
+            "
+            data-toggle="popover"
+            data-trigger="click"
+            href="#"
+            class="mx-1"
+            @click.prevent
+          >
+            <font-awesome-icon icon="info-circle" />
+          </a>
         </template>
-      </span>
-      <template v-if="longpost">
-        <button
-          class="btn btn-link mr-3 btn-primary"
-          data-test-collapse-button
-          style="margin-top:.8em;display:block"
-          type="button"
-          @click="toggleLongpost">
-          <font-awesome-icon
-            icon="plus"
-            aria-hidden="true"/> Expand Post
-        </button>
+        <emojify
+          v-else
+          :text="entity.text"
+          :key="`text-${i}`" />
       </template>
-    </template>
-    <template v-else>
-      <emojify
-        v-if="longpost.title"
-        :text="longpost.title"
-        element="h5"
-      />
-      <emojify
-        :text="longpost.body"
-        element="p"
-      />
-      <div style="margin-top:.8em">
-        <button
-          class="btn btn-link mr-3 btn-primary"
-          type="button"
-          @click="toggleLongpost">
-          <font-awesome-icon
-            icon="minus"
-            aria-hidden="true"/> Collapse Post
-        </button>
-      </div>
-    </template>
+    </span>
   </span>
-  <span v-else>
-    <slot />
-  </span>
+  <span v-else> <slot /> </span>
 </template>
 <script>
 import unicodeSubstring from 'unicode-substring'
@@ -115,20 +71,6 @@ export default {
     deleted: {
       type: Boolean,
       default: false
-    },
-    spoiler: {
-      type: Object,
-      default: null
-    },
-    longpost: {
-      type: Object,
-      default: null
-    }
-  },
-  data() {
-    return {
-      showSpoiler: false,
-      showLongpost: false
     }
   },
   computed: {
@@ -181,12 +123,6 @@ export default {
   },
   methods: {
     unicodeSubstring,
-    toggleSpoiler() {
-      this.showSpoiler = !this.showSpoiler
-    },
-    toggleLongpost() {
-      this.showLongpost = !this.showLongpost
-    },
     replaceLinkText(entity) {
       const text = unicodeSubstring(entity.text, 0, entity.len)
       const isURLLiteral = entity.link === entity.text
