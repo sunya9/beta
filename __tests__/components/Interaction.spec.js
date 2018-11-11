3982de3b61019d7db065062de0a633fe71d7652e
sunya9 <mugen.xyz@gmail.com>
2018-11-12 00:28:28 +0900

    Fix interaction test

--- __tests__/components/Interaction.spec.js
+++ __tests__/components/Interaction.spec.js
@@ -1,55 +1,57 @@
 import Interaction from '~/components/Interaction'
-import Post from '~/components/Post'
-import { mount, authedUserCreateStore, baseMountOpts, fixtures } from 'helper'
+import { mount, authedUserCreateStore, fixtures } from 'helper'
 
 describe('Interaction component', () => {
-  let wrapper
-  beforeEach(() => {
-    wrapper = mount(
-      Interaction,
-      baseMountOpts({
-        mocks: {
-          $store: authedUserCreateStore()
-        },
-        stubs: {
-          Post
-        },
-        propsData: {
-          interaction: fixtures('interaction') // dummy
-        }
-      })
-    )
-  })
-  test('reply', () => {
-    wrapper.setProps({
-      interaction: fixtures('interaction', 'reply')
+  it('reply', () => {
+    const wrapper = mount(Interaction, {
+      mocks: {
+        $store: authedUserCreateStore()
+      },
+      propsData: {
+        interaction: fixtures('interaction', 'reply')
+      }
     })
     const text = wrapper.text()
     expect(text).toContain('replied') // message
     expect(text).toContain('body') // reply body
   })
 
-  test('bookmark', () => {
-    wrapper.setProps({
-      interaction: fixtures('interaction', 'bookmark')
+  it('bookmark', () => {
+    const wrapper = mount(Interaction, {
+      mocks: {
+        $store: authedUserCreateStore()
+      },
+      propsData: {
+        interaction: fixtures('interaction', 'bookmark')
+      }
     })
     const text = wrapper.text()
     expect(text).toContain('starred') // message
     expect(text).toContain('body') // bookmark body
   })
 
-  test('repost', () => {
-    wrapper.setProps({
-      interaction: fixtures('interaction', 'repost')
+  it('repost', () => {
+    const wrapper = mount(Interaction, {
+      mocks: {
+        $store: authedUserCreateStore()
+      },
+      propsData: {
+        interaction: fixtures('interaction', 'repost')
+      }
     })
     const text = wrapper.text()
     expect(text).toContain('reposted') // message
     expect(text).toContain('body') // repost body
   })
 
-  test('follow', () => {
-    wrapper.setProps({
-      interaction: fixtures('interaction', 'follow')
+  it('follow', () => {
+    const wrapper = mount(Interaction, {
+      mocks: {
+        $store: authedUserCreateStore()
+      },
+      propsData: {
+        interaction: fixtures('interaction', 'follow')
+      }
     })
     const text = wrapper.text()
     expect(text).toContain('Followed') // message
