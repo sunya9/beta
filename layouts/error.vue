19f747e1a3fdbfb7a4e74fd32231cb50ebfec312
sunya9 <mugen.xyz@gmail.com>
2018-11-21 05:53:20 +0900

    Fix title

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="container text-center">
-    <h1>
-      {{ error.statusCode }}
-    </h1>
-    <p class="lead">
-      {{ error.message }}
-    </p>
+    <h1>{{ error.statusCode }}</h1>
+    <p class="lead">{{ error.message }}</p>
     <nuxt-link
       v-if="error.statusCode"
       :to="error.home || '/'"
-      class="btn btn-primary">
+      class="btn btn-primary"
+    >
       Top
     </nuxt-link>
   </div>
@@ -22,6 +19,11 @@ export default {
       default: null
     }
   },
-  layout: 'no-sidebar'
+  layout: 'no-sidebar',
+  head() {
+    return {
+      title: this.error.statusCode
+    }
+  }
 }
 </script>
