4eab2d8f879a489f657db32ea0e1e86c8c4ba5bb
sunya9 <mugen.xyz@gmail.com>
2019-02-23 03:01:56 +0900

    Remove comment

--- plugins/mousetrap.js
+++ plugins/mousetrap.js
@@ -1,24 +1,6 @@
 import Mousetrap from 'mousetrap'
 require('imports-loader?Mousetrap=>require("mousetrap")!mousetrap/plugins/pause/mousetrap-pause') // eslint-disable-line
 
-// if (process.browser) {
-
-//   module.exports = {
-//     pause() {
-//       Mousetrap.pause() // eslint-disable-line
-//     },
-//     unpause() {
-//       Mousetrap.unpause() // eslint-disable-line
-//     },
-//     bind(...args) {
-//       Mousetrap.bind(...args) // eslint-disable-line
-//     },
-//     unbind(...args) {
-//       Mousetrap.unbind(...args) // eslint-disable-line
-//     }
-//   }
-// }
-
 export default (app, inject) => {
   inject('mousetrap', Mousetrap)
 }
