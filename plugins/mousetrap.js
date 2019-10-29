813d2891dc224ff32b17d2dd18e4666ee5e991b0
sunya9 <mugen.xyz@gmail.com>
2019-10-30 04:05:31 +0900

    Fix variable name

--- plugins/mousetrap.js
+++ plugins/mousetrap.js
@@ -1,6 +1,6 @@
 import Mousetrap from 'mousetrap'
 require('imports-loader?Mousetrap=>require("mousetrap")!mousetrap/plugins/pause/mousetrap-pause') // eslint-disable-line
 
-export default (app, inject) => {
+export default (_, inject) => {
   inject('mousetrap', Mousetrap)
 }
