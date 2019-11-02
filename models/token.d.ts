bb207eee9f307dc8799ef01dfdb958aa7c2c5874
sunya9 <mugen.xyz@gmail.com>
2019-11-03 02:07:58 +0900

    Convert code of store to typescript

---
+++ models/token.d.ts
@@ -0,0 +1,30 @@
+import { User } from './user'
+import { Client } from './client'
+
+export interface Token {
+  app: Client.Source;
+  scopes: Token.Scope[];
+  user: User;
+  storage: Token.Storage;
+}
+
+export namespace Token {
+  export enum Scope {
+    basic,
+    stream,
+    write_post,
+    follow,
+    update_profile,
+    presence,
+    messages,
+    'messages:io.pnut.core.pm',
+    public_messages,
+    files,
+    polls,
+    email
+  }
+  export interface Storage {
+    available: number;
+    total: number;
+  }
+}
