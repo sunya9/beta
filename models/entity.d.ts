673c0cc6a48c0ac43658331e4961bed33d81612c
sunya9 <mugen.xyz@gmail.com>
2019-11-06 09:44:15 +0900

    Fix entity type

--- models/entity.d.ts
+++ models/entity.d.ts
@@ -1,16 +1,28 @@
 export interface Entity {
-  amended_len: number;
-  link: string;
   text: string;
   len: number;
   pos: number;
-  title?: string;
 }
 
 export namespace Entity {
+  export interface Link extends Entity {
+    title?: string;
+    amended_len: number;
+    link: string;
+  }
+  export interface Mention extends Entity {
+    id: string;
+    is_copy: boolean;
+    is_leading: boolean;
+  }
+  export type Tag = Entity
   export interface HaveEntity {
     text: string;
     html?: string;
-    entity: Entity;
+    entities: {
+      links: Link[];
+      mentions: Mention[];
+      tags: Tag[];
+    };
   }
 }
