a55485a0942fb62d949bc1238a1cb485c5050f37
sunya9 <mugen.xyz@gmail.com>
2019-12-20 05:00:41 +0900

    Fix eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,12 @@ module.exports = {
     node: true
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vue/recommended',
-    'plugin:jest/recommended',
     '@nuxtjs/eslint-config-typescript',
+    'eslint:recommended',
+    'plugin:jest/recommended',
     'prettier'
   ],
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  plugins: ['@typescript-eslint', 'vue', 'jest'],
+  plugins: ['jest'],
   // add your custom rules here
   rules: {
     'no-console': [
@@ -35,10 +27,10 @@ module.exports = {
       }
     ],
     camelcase: 'off',
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
+    // '@typescript-eslint/camelcase': 'off',
+    // '@typescript-eslint/explicit-function-return-type': 'off',
     eqeqeq: 'error',
-    'prefer-template': 'error',
+    // 'prefer-template': 'error',
     // https://teppeis.hatenablog.com/entry/2019/02/typescript-eslint
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
