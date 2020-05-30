227628742ac2967416c39c20336c5fcfdba53319
sunya9 <mugen.xyz@gmail.com>
2020-05-31 07:54:48 +0900

    refactor: move webpackconfig

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const merge = require('webpack-merge')
 
+const srcDir = '../src'
+const rootDir = '../'
+
 module.exports = {
   stories: ['../src/**/*.stories.ts'],
   addons: [
@@ -9,55 +12,55 @@ module.exports = {
     '@storybook/addon-backgrounds/register',
     '@storybook/theming',
   ],
-  webpackFinal: (storybookBaseConfig, configType) => {
-    const srcDir = '../src'
-    const rootDir = '../'
-    return merge(storybookBaseConfig, {
-      resolve: {
-        alias: {
-          '~~': path.resolve(__dirname, rootDir),
-          '~': path.resolve(__dirname, srcDir),
-        },
-        extensions: ['.ts', '.tsx'],
-      },
-      module: {
-        rules: [
+  webpackFinal: (storybookBaseConfig) => {
+    return merge(storybookBaseConfig, webpackConfig)
+  },
+}
+
+const webpackConfig = {
+  resolve: {
+    alias: {
+      '~~': path.resolve(__dirname, rootDir),
+      '~': path.resolve(__dirname, srcDir),
+    },
+    extensions: ['.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
           {
-            test: /\.ts$/,
-            use: [
-              {
-                loader: 'ts-loader',
-                options: {
-                  appendTsSuffixTo: [/\.vue$/],
-                  transpileOnly: true,
-                },
-              },
-            ],
-          },
-          {
-            test: /\.(otf|eot|ttf|woff|woff2)(\?.+)?$/,
-            loader: 'url-loader',
-          },
-          // {
-          //   test: /\.svg$/,
-          //   loader: 'file-loader',
-          // },
-          {
-            test: /\.(sass|scss)$/,
-            loaders: ['style-loader', 'css-loader', 'sass-loader'],
-          },
-          {
-            test: /\.vue$/,
-            loader: 'vue-docgen-loader',
-            enforce: 'post',
-          },
-          {
-            test: /\.vue$/,
-            use: 'vue-docgen-loader',
-            enforce: 'post',
+            loader: 'ts-loader',
+            options: {
+              appendTsSuffixTo: [/\.vue$/],
+              transpileOnly: true,
+            },
           },
         ],
       },
-    })
+      {
+        test: /\.(otf|eot|ttf|woff|woff2)(\?.+)?$/,
+        loader: 'url-loader',
+      },
+      // {
+      //   test: /\.svg$/,
+      //   loader: 'file-loader',
+      // },
+      {
+        test: /\.(sass|scss)$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-docgen-loader',
+        enforce: 'post',
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-docgen-loader',
+        enforce: 'post',
+      },
+    ],
   },
 }
