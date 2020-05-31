62677a9a995019b28c46b749021b7de05a49153a
sunya9 <mugen.xyz@gmail.com>
2020-05-31 09:21:23 +0900

    feat: add storycap

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,11 @@
+import { configure, addDecorator } from '@storybook/vue'
+import { withScreenshot } from 'storycap'
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 import '../src/assets/css/main.scss'
 import './css/main.css'
+
 Vue.use(Vuex)
+
+addDecorator(withScreenshot())
