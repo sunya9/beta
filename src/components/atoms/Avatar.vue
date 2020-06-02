7acb002bd1f323cd3681ef602c7bf4f89ffb87a5
sunya9 <mugen.xyz@gmail.com>
2020-06-02 09:52:40 +0900

    refactor: change to class component

--- src/components/atoms/Avatar.vue
+++ src/components/atoms/Avatar.vue
@@ -1,6 +1,5 @@
 <template>
   <img
-    :src="src"
     :class="{
       'rounded-circle': !isSquare,
       'is-deleted': isDeletedUser,
@@ -13,75 +12,82 @@
 </template>
 
 <script lang="ts">
-import Vue, { PropOptions } from 'vue'
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
 import { User } from '~/models/user'
 import deletedUser from '~/assets/img/beta.svg'
 
 function sizeValidator(numLike: string) {
-  return [0, 16, 24, 32, 64, 96].includes(parseInt(numLike))
+  return [0, 16, 24, 32, 48, 64, 96].includes(parseInt(numLike))
 }
 
 function avatarIsString(avatar: User.UserImage | string): avatar is string {
   return typeof avatar === 'string'
 }
-export default Vue.extend({
-  name: 'Avatar',
-  props: {
-    avatar: {
-      type: [Object, String],
-      required: false,
-      validator(obj) {
-        return obj === null || typeof obj === 'string' || 'link' in obj
-      },
-      default: '',
-    } as PropOptions<User.UserImage | string | null>,
-    enablePlaceholder: {
-      type: Boolean,
-      default: false,
+
+@Component({})
+export default class Avatar extends Vue {
+  @Prop({
+    type: [Object, String],
+    required: false,
+    validator(obj) {
+      return obj === null || typeof obj === 'string' || 'link' in obj
     },
-    size: {
-      type: [String, Number],
-      default: 24,
-      validator: sizeValidator,
-    } as PropOptions<number | string>,
-    maxSize: {
-      type: [String, Number],
-      default: 0,
-      validator: sizeValidator,
-    } as PropOptions<number | string>,
-  },
-  data() {
-    return {
-      isSquare: false,
-    }
-  },
-  computed: {
-    url(): string {
-      if (!this.avatar || this.enablePlaceholder) return ''
-      return avatarIsString(this.avatar)
-        ? /^(@\w+|\d+)$/.test(this.avatar)
-          ? `https://api.pnut.io/v0/users/${this.avatar}/avatar`
-          : this.avatar
-        : this.avatar.link
-      // ''
-    },
-    isDeletedUser(): boolean {
-      return !this.avatar
-    },
-    src(): string {
-      if (!this.url) return deletedUser
-      let src = this.url
-      if (this.maxSize > 0) src += `?w=${this.maxSize}`
-      return src
-    },
-    srcset(): string {
-      return `${this.url}?w=${(+this.size || +this.maxSize) * 2} 2x`
-    },
-  },
+    default: '',
+  })
+  avatar!: User.UserImage | string | null
+
+  @Prop({
+    type: Boolean,
+    default: false,
+  })
+  enablePlaceholder!: boolean
+
+  @Prop({
+    type: [String, Number],
+    default: 24,
+    validator: sizeValidator,
+  })
+  size!: number | string
+
+  @Prop({
+    type: [String, Number],
+    default: 0,
+    validator: sizeValidator,
+  })
+  maxSize!: number | string
+
+  isSquare = false
+
+  get url(): string {
+    if (!this.avatar || this.enablePlaceholder) return ''
+    return avatarIsString(this.avatar)
+      ? /^(@\w+|\d+)$/.test(this.avatar)
+        ? `https://api.pnut.io/v0/users/${this.avatar}/avatar`
+        : this.avatar
+      : this.avatar.link
+    // ''
+  }
+
+  get isDeletedUser(): boolean {
+    return !this.avatar
+  }
+
+  get src(): string {
+    if (!this.url) return deletedUser
+    let src = this.url
+    if (this.maxSize > 0) src += `?w=${this.maxSize}`
+    return src
+  }
+
+  get srcset(): string {
+    return `${this.url}?w=${(+this.size || +this.maxSize) * 2} 2x`
+  }
+
   mounted() {
     this.isSquare = localStorage.getItem('square_avatars') === 'true'
-  },
-})
+  }
+}
 </script>
 <style scoped>
 .is-deleted {
