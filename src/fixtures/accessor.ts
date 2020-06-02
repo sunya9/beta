866d5199dcf43c1b5985133d8f9fd7b838b7283b
sunya9 <mugen.xyz@gmail.com>
2020-06-03 05:47:31 +0900

    refactor: remove duplication code

---
+++ src/fixtures/accessor.ts
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import { getAccessorType } from 'nuxt-typed-vuex'
+import { DeepPartial } from '~/../types'
+
+export function assignAccessor(accessor?: DeepPartial<typeof getAccessorType>) {
+  Vue.prototype.$accessor = {
+    ...accessor,
+    user: { username: 'test' },
+    storage: { available: 1, total: 1 },
+  }
+}
