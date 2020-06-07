765ca1eff6482e63af04e534b1895704c8ad39f3
sunya9 <mugen.xyz@gmail.com>
2020-06-08 06:51:12 +0900

    refactor: rename some usecases

--- src/plugins/domain/usecases/createPoll.ts
+++ src/plugins/domain/usecases/createPoll.ts
@@ -1,19 +1,19 @@
 import { Usecase } from '~/plugins/domain/usecases/usecase'
 import { PnutRepository } from '~/plugins/domain/repository/pnutRepository'
-import { PostPollRequest } from '~/plugins/domain/dto/poll'
+import { CreatePollRequest } from '~/plugins/domain/dto/poll'
 import { Poll } from '~/models/poll'
 
 interface Input {
-  poll: PostPollRequest
+  poll: CreatePollRequest
   fallbackText?: string
 }
 
 interface Output {
   poll: Poll
 }
-export interface PostPollUseCase extends Usecase<Input, Promise<Output>> {}
+export interface CreatePollUseCase extends Usecase<Input, Promise<Output>> {}
 
-export class PostPollInteractor implements PostPollUseCase {
+export class CreatePollInteractor implements CreatePollUseCase {
   constructor(private readonly pnutRepository: PnutRepository) {}
   async run(input: Input): Promise<Output> {
     const res = await this.pnutRepository.postPoll(
