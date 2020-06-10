0f74a922d1a643f8aa6f4c8ff733c413667951c0
sunya9 <mugen.xyz@gmail.com>
2020-06-11 07:52:06 +0900

    refactor: move compose logic to ComposeAbstract

--- src/components/organisms/Compose.vue
+++ src/components/organisms/Compose.vue
@@ -36,20 +36,14 @@
             />
           </client-only>
         </div>
-        <div v-show="photos.length" class="form-group">
-          <transition-group
-            tag="div"
-            name="photos"
-            class="d-flex flex-wrap justify-content align-items-center"
-          >
-            <thumb
-              v-for="(photo, i) in previewPhotos"
-              :key="photo"
-              :original="photo"
-              :thumb="photo"
-              removable
+        <div v-show="files.length" class="form-group">
+          <transition-group tag="div" name="file" class="d-flex flex-wrap">
+            <file-preview
+              v-for="(fileWrapper, i) in fileWrappers"
+              :key="fileWrapper.id"
+              :file="fileWrapper.file"
               class="mr-2"
-              @remove="photos.splice(i, 1)"
+              @remove="fileWrappers.splice(i, 1)"
             />
           </transition-group>
         </div>
@@ -62,11 +56,11 @@
               v-show="!noPhoto"
               v-if="storage.available"
               :disabled="promise"
-              class="btn btn-link text-dark add-photo mr-2"
+              class="btn btn-link text-dark mb-0 mr-2"
             >
               <font-awesome-icon :icon="['far', 'image']" />
               <span class="d-none d-lg-inline ml-2">
-                Photo
+                File
               </span>
               <input
                 ref="file"
@@ -105,34 +99,12 @@
                 Spoiler
               </span>
             </button>
-            <button
-              :class="{
-                'text-dark': !hasLongpost,
-                'btn-primary': hasLongpost,
-              }"
-              class="btn btn-link add-longpost mr-2"
-              type="button"
-              @click="toggleLongpost"
-            >
-              <font-awesome-icon icon="plus" />
-              <span class="d-none d-lg-inline ml-2">
-                Long
-              </span>
-            </button>
-            <button
-              :class="{
-                'text-dark': !nsfw,
-                'btn-primary': nsfw,
-              }"
-              class="btn btn-link toggle-nsfw mr-2"
-              type="button"
-              @click="toggleNsfw"
-            >
-              <font-awesome-icon icon="exclamation-circle" />
-              <span class="d-none d-lg-inline ml-2">
-                NSFW
-              </span>
-            </button>
+            <toggle-longpost
+              class="mr-2"
+              :value="!!longpost"
+              @input="longpost = $event"
+            />
+            <toggle-nsfw v-model="nsfw" class="mr-2" />
             <button
               :disabled="disabled"
               type="submit"
@@ -162,27 +134,20 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop, Watch } from 'nuxt-property-decorator'
+import { Component, Prop, Watch } from 'nuxt-property-decorator'
 import { Mixins } from 'vue-property-decorator'
-import { CreatePollRequest } from '~/plugins/domain/dto/poll'
-import { Token } from '~/models/token'
+import { createCompose } from './ComposeAbstract'
 import { Post } from '~/models/post'
-import { LongPost } from '~/models/raw/raw/long-post'
-import { Spoiler } from '~/models/raw/raw/spoiler'
-import { User } from '~/models/user'
-import {
-  getMinimumPoll,
-  getMinimumSpoiler,
-  getMinimumLongPost,
-} from '~/util/minimum-entities'
 import bus from '~/assets/ts/bus'
 import Thumb from '~/components/Thumb.vue'
 import { Picker } from '~/plugins/emoji'
 import InputPoll from '~/components/InputPoll.vue'
 import InputSpoiler from '~/components/InputSpoiler.vue'
 import InputLongpost from '~/components/InputLongpost.vue'
-import textCount from '~/assets/ts/text-count'
 import resettable from '~/assets/ts/resettable'
+import ToggleNsfw from '~/components/atoms/ToggleNsfw.vue'
+import ToggleLongpost from '~/components/atoms/ToggleLongpost.vue'
+import FilePreview from '~/components/molecules/FilePreview/FilePreview.vue'
 
 @Component({
   components: {
@@ -191,21 +156,15 @@ import resettable from '~/assets/ts/resettable'
     InputPoll,
     InputSpoiler,
     InputLongpost,
+    ToggleNsfw,
+    ToggleLongpost,
+    FilePreview,
   },
 })
-export default class Composer extends Mixins(textCount, resettable) {
-  @Prop({
-    type: String,
-    default: '',
-  })
-  initialText!: string
-
-  @Prop({
-    type: Boolean,
-    default: false,
-  })
-  focus!: boolean
-
+export default class Compose extends Mixins(
+  resettable,
+  createCompose({ textCount: 256 })
+) {
   @Prop({
     type: Object,
     default: null,
@@ -236,109 +195,6 @@ export default class Composer extends Mixins(textCount, resettable) {
     default: null,
   })
   editPost!: Post | null
-
-  promise: Promise<any> | null = null
-  photos: File[] = []
-  previewPhotos: string[] = []
-  text: string = this.initialText
-  replyStartPos = 0
-  showEmojiPicker = false
-  poll: CreatePollRequest | null = null
-
-  spoiler: Spoiler.Value | null = null
-  longpost: LongPost.Value | null = null
-  nsfw = false
-
-  get hasPoll(): boolean {
-    return !!this.poll
-  }
-
-  get hasSpoiler(): boolean {
-    return !!this.spoiler
-  }
-
-  get hasLongpost(): boolean {
-    return !!this.longpost
-  }
-
-  get postCounter(): number {
-    return 256 - this.textLength
-  }
-
-  get textOverflow(): boolean {
-    return this.postCounter < 0
-  }
-
-  get hasNoText(): boolean {
-    return !this.textLength
-  }
-
-  get disabled(): boolean {
-    const sending = !!this.promise
-    return !!(
-      this.textOverflow ||
-      this.hasNoText ||
-      sending ||
-      (this.poll && !this.availablePoll) ||
-      (this.spoiler && !this.availableSpoiler) ||
-      (this.longpost && !this.availableLongpost)
-    )
-  }
-
-  get availablePoll(): boolean {
-    return (
-      !!this.poll &&
-      this.poll.options &&
-      this.poll.options.filter((option) => option.text).length >= 2
-    )
-  }
-
-  get availableSpoiler(): boolean {
-    return (
-      !!this.spoiler &&
-      !!this.spoiler.topic &&
-      this.spoiler.topic.length > 0 &&
-      this.spoiler.topic.length <= 128
-    )
-  }
-
-  get availableLongpost(): boolean {
-    return (
-      !!this.longpost &&
-      !!this.longpost.body &&
-      this.longpost.body.length > 0 &&
-      this.longpost.body.length <= 6144 &&
-      (!this.longpost.title || this.longpost.title.length < 128)
-    )
-  }
-
-  get user(): User | null {
-    return this.$accessor.user
-  }
-
-  get storage(): Token.Storage {
-    return this.$accessor.storage
-  }
-
-  @Watch('photos')
-  async onChangephotos() {
-    const promisePhotos = this.photos.map((file) => {
-      return new Promise<string>((resolve) => {
-        const fr = new FileReader()
-        fr.readAsDataURL(file)
-        fr.onload = (e) => {
-          if (
-            !e.target ||
-            !e.target.result ||
-            typeof e.target.result !== 'string'
-          )
-            throw new Error('Failed to load photo')
-          resolve(e.target.result)
-        }
-      })
-    })
-    this.previewPhotos = await Promise.all(promisePhotos)
-  }
 
   @Watch('replyTarget', { immediate: true })
   onChangereplyTarget(replyTarget: Post) {
@@ -374,93 +230,6 @@ export default class Composer extends Mixins(textCount, resettable) {
     this.text = editPost ? editPost.content.text : ''
   }
 
-  created() {
-    this.text = this.initialText
-  }
-
-  mounted() {
-    if (this.focus) {
-      this.setFocus()
-    }
-  }
-
-  toggleNsfw() {
-    this.nsfw = !this.nsfw
-  }
-
-  insertText(text: string) {
-    const textarea = this.$refs.textarea
-    const getSelection = document.getSelection()
-    if (getSelection) {
-      textarea.focus()
-
-      const sel = (getSelection as any).createRange()
-      sel.text = text
-      textarea.focus()
-    } else if (textarea.selectionStart || textarea.selectionStart === 0) {
-      const startPos = textarea.selectionStart
-      const endPos = textarea.selectionEnd
-
-      const scrollTop = textarea.scrollTop
-      const updateText =
-        textarea.value.substring(0, startPos) +
-        text +
-        textarea.value.substring(endPos, textarea.value.length)
-      this.text = updateText
-
-      this.$nextTick(() => {
-        textarea.focus()
-        textarea.selectionStart = startPos + text.length
-        textarea.selectionEnd = startPos + text.length
-        textarea.scrollTop = scrollTop
-      })
-    } else {
-      this.text = text
-      textarea.focus()
-    }
-  }
-
-  togglePoll() {
-    this.poll = this.poll ? null : getMinimumPoll()
-  }
-
-  toggleSpoiler() {
-    this.spoiler = this.spoiler ? null : getMinimumSpoiler()
-  }
-
-  toggleLongpost() {
-    this.longpost = this.longpost ? null : getMinimumLongPost()
-  }
-
-  getSheet() {
-    return require('emoji-datasource-twitter/img/twitter/sheets-128/64.png')
-  }
-
-  setFocus(force = false) {
-    if (this.focus === false && !force) return
-    // occur error if it not displayed like logged out
-    // TODO
-
-    const textarea = this.$refs.textarea as any
-    // TODO
-    // if (this.text.length === undefined) {
-    //   this.text.length = this.replyStartPos
-    // }
-    if ('selectionStart' in textarea) {
-      textarea.selectionStart = this.replyStartPos
-      textarea.selectionEnd = this.text.length
-    } else if (textarea.setSelectionRange) {
-      textarea.setSelectionRange(this.replyStartPos, this.text.length)
-    } else if (textarea.createTextRange) {
-      const range = textarea.createTextRange()
-      range.collapse(true)
-      range.moveEnd('character', this.text.length)
-      range.moveStart('character', this.replyStartPos)
-      range.select()
-    }
-    textarea.focus()
-  }
-
   createPost() {
     return this.$interactors.createPost
       .run({
@@ -469,7 +238,7 @@ export default class Composer extends Mixins(textCount, resettable) {
         reply: this.replyTarget,
         longpost: this.longpost,
         spoiler: this.spoiler,
-        files: this.photos,
+        files: this.files,
         pollRequest: this.poll,
       })
       .then((res) => {
@@ -520,54 +289,6 @@ export default class Composer extends Mixins(textCount, resettable) {
       this.promise = null
     }
   }
-
-  initialize() {
-    this.text = ''
-    this.photos = []
-    this.poll = null
-    this.spoiler = null
-    this.longpost = null
-    this.nsfw = false
-  }
-
-  fileChange(e: Event) {
-    const { target } = e
-    if (
-      !(target instanceof HTMLInputElement) ||
-      !target.files ||
-      !target.files.length
-    )
-      return
-    const newPhotos = Array.from(target.files)
-    this.photos = [...this.photos, ...newPhotos]
-    // reset file form for detecting changes(if there `sn't below code, not working when is selected same file)
-    this.$refs.file.value = ''
-  }
-
-  // TODO
-  addEmoji(emoji: any) {
-    this.insertText(emoji.native)
-    this.closeEmojiPalette()
-  }
-
-  $refs!: {
-    picker: Vue
-    textarea: HTMLTextAreaElement
-    file: HTMLInputElement
-  }
-
-  async toggleEmojiPalette() {
-    this.showEmojiPicker = !this.showEmojiPicker
-    if (!this.showEmojiPicker) return
-    await this.$nextTick()
-    const input = this.$refs.picker.$el.querySelector('input')
-    if (!input) return
-    input.focus()
-  }
-
-  closeEmojiPalette() {
-    this.showEmojiPicker = false
-  }
 }
 </script>
 
@@ -586,19 +307,18 @@ export default class Composer extends Mixins(textCount, resettable) {
     font-size: 16px;
   }
 }
-
-.photos-enter-active,
-.photos-leave-to {
+.file-enter-active,
+.file-leave-to {
   transition: all 0.5s ease;
 }
 
-.photos-enter,
-.photos-leave-to {
+.file-enter,
+.file-leave-to {
   opacity: 0;
   transform: scale(0);
 }
 
-.photos-move {
+.file-move {
   transition: transform 0.5s;
 }
 
@@ -622,10 +342,5 @@ export default class Composer extends Mixins(textCount, resettable) {
   right: 0;
   top: 2rem;
   z-index: 3;
-}
-
-.add-photo {
-  cursor: pointer;
-  margin-bottom: 0;
 }
 </style>
---
+++ src/components/organisms/ComposeAbstract.ts
@@ -0,0 +1,247 @@
+import Vue from 'vue'
+import { Component, Prop, Mixins } from 'vue-property-decorator'
+import { User } from '~/models/user'
+import { Spoiler } from '~/models/raw/raw/spoiler'
+import { LongPost } from '~/models/raw/raw/long-post'
+import textCount from '~/assets/ts/text-count'
+import { getMinimumPoll, getMinimumSpoiler } from '~/util/minimum-entities'
+import { CreatePollRequest } from '~/plugins/domain/dto/poll'
+import { Token } from '~/models/token'
+
+interface FileWrapper {
+  file: File
+  id: string
+}
+
+interface ComposeOptions {
+  textCount: number
+}
+
+export function createCompose(composeOptions: ComposeOptions) {
+  @Component
+  class ComposeAbstract extends Mixins(textCount) {
+    @Prop({
+      type: String,
+      default: '',
+    })
+    initialText!: string
+
+    @Prop({
+      type: Boolean,
+      default: false,
+    })
+    focus!: boolean
+
+    fileWrappers: FileWrapper[] = []
+    promise: Promise<any> | null = null
+    text: string = this.initialText
+    replyStartPos = 0
+    showEmojiPicker = false
+
+    spoiler: Spoiler.Value | null = null
+    longpost: LongPost.Value | null = null
+    nsfw = false
+
+    get postCounter(): number {
+      return composeOptions.textCount - this.textLength
+    }
+
+    get textOverflow(): boolean {
+      return this.postCounter < 0
+    }
+
+    get hasNoText(): boolean {
+      return !this.textLength
+    }
+
+    get files(): File[] {
+      return this.fileWrappers.map((fileWrapper) => fileWrapper.file)
+    }
+
+    initialize() {
+      this.fileWrappers = []
+      this.text = ''
+      this.poll = null
+      this.spoiler = null
+      this.longpost = null
+      this.nsfw = false
+    }
+
+    $refs!: {
+      picker: Vue
+      textarea: HTMLTextAreaElement
+      file: HTMLInputElement
+    }
+
+    fileChange(e: Event) {
+      const { target } = e
+      if (!(target instanceof HTMLInputElement) || !target.files?.length) return
+      const newFileWrappers: FileWrapper[] = Array.from(
+        target.files
+      ).map((file, i) => ({ file, id: `${new Date().getTime()}-${i}` }))
+      this.fileWrappers = [...this.fileWrappers, ...newFileWrappers]
+      // reset file form for detecting changes(if there `sn't below code, not working when is selected same file)
+      this.$refs.file.value = ''
+    }
+
+    get user(): User | null {
+      return this.$accessor.user
+    }
+
+    get hasPoll(): boolean {
+      return !!this.poll
+    }
+
+    get hasSpoiler(): boolean {
+      return !!this.spoiler
+    }
+
+    get hasLongpost(): boolean {
+      return !!this.longpost
+    }
+
+    poll: CreatePollRequest | null = null
+
+    get disabled(): boolean {
+      const sending = !!this.promise
+      return !!(
+        this.textOverflow ||
+        this.hasNoText ||
+        sending ||
+        (this.poll && !this.availablePoll) ||
+        (this.spoiler && !this.availableSpoiler) ||
+        (this.longpost && !this.availableLongpost)
+      )
+    }
+
+    get availablePoll(): boolean {
+      return (
+        !!this.poll &&
+        this.poll.options &&
+        this.poll.options.filter((option) => option.text).length >= 2
+      )
+    }
+
+    get availableSpoiler(): boolean {
+      return (
+        !!this.spoiler &&
+        !!this.spoiler.topic &&
+        this.spoiler.topic.length > 0 &&
+        this.spoiler.topic.length <= 128
+      )
+    }
+
+    get availableLongpost(): boolean {
+      return (
+        !!this.longpost &&
+        !!this.longpost.body &&
+        this.longpost.body.length > 0 &&
+        this.longpost.body.length <= 6144 &&
+        (!this.longpost.title || this.longpost.title.length < 128)
+      )
+    }
+
+    mounted() {
+      if (this.focus) {
+        this.setFocus()
+      }
+    }
+
+    created() {
+      this.text = this.initialText
+    }
+
+    setFocus(force = false) {
+      if (this.focus === false && !force) return
+      // occur error if it not displayed like logged out
+      // TODO
+
+      const textarea = this.$refs.textarea as any
+      // TODO
+      // if (this.text.length === undefined) {
+      //   this.text.length = this.replyStartPos
+      // }
+      if ('selectionStart' in textarea) {
+        textarea.selectionStart = this.replyStartPos
+        textarea.selectionEnd = this.text.length
+      } else if (textarea.setSelectionRange) {
+        textarea.setSelectionRange(this.replyStartPos, this.text.length)
+      } else if (textarea.createTextRange) {
+        const range = textarea.createTextRange()
+        range.collapse(true)
+        range.moveEnd('character', this.text.length)
+        range.moveStart('character', this.replyStartPos)
+        range.select()
+      }
+      textarea.focus()
+    }
+
+    togglePoll() {
+      this.poll = this.poll ? null : getMinimumPoll()
+    }
+
+    toggleSpoiler() {
+      this.spoiler = this.spoiler ? null : getMinimumSpoiler()
+    }
+
+    getSheet() {
+      return require('emoji-datasource-twitter/img/twitter/sheets-128/64.png')
+    }
+
+    insertText(text: string) {
+      const textarea = this.$refs.textarea
+      const getSelection = document.getSelection()
+      if (getSelection) {
+        textarea.focus()
+
+        const sel = (getSelection as any).createRange()
+        sel.text = text
+        textarea.focus()
+      } else if (textarea.selectionStart || textarea.selectionStart === 0) {
+        const startPos = textarea.selectionStart
+        const endPos = textarea.selectionEnd
+
+        const scrollTop = textarea.scrollTop
+        const updateText =
+          textarea.value.substring(0, startPos) +
+          text +
+          textarea.value.substring(endPos, textarea.value.length)
+        this.text = updateText
+
+        this.$nextTick(() => {
+          textarea.focus()
+          textarea.selectionStart = startPos + text.length
+          textarea.selectionEnd = startPos + text.length
+          textarea.scrollTop = scrollTop
+        })
+      } else {
+        this.text = text
+        textarea.focus()
+      }
+    }
+
+    // TODO
+    addEmoji(emoji: any) {
+      this.insertText(emoji.native)
+      this.closeEmojiPalette()
+    }
+
+    async toggleEmojiPalette() {
+      this.showEmojiPicker = !this.showEmojiPicker
+      if (!this.showEmojiPicker) return
+      await this.$nextTick()
+      const input = this.$refs.picker.$el.querySelector('input')
+      if (!input) return
+      input.focus()
+    }
+
+    closeEmojiPalette() {
+      this.showEmojiPicker = false
+    }
+
+    get storage(): Token.Storage {
+      return this.$accessor.storage
+    }
+  }
+  return ComposeAbstract
+}
