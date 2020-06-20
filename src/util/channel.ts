574519f390976f559fc06a8d3183fc7045b27ad0
sunya9 <mugen.xyz@gmail.com>
2020-06-21 06:00:09 +0900

    fix: wrong comparison

--- src/util/channel.ts
+++ src/util/channel.ts
@@ -3,11 +3,11 @@ import { Channel } from '~/models/channel'
 export function userIdsIsString(
   userIds: Array<string | Channel.SimpleUser>
 ): userIds is string[] {
-  return userIds.every((item) => typeof item === 'string')
+  return !!userIds.length && userIds.every((item) => typeof item === 'string')
 }
 
 export function userIdIsSimpleUser(
   userId: string | Channel.SimpleUser
 ): userId is Channel.SimpleUser {
-  return typeof userId === 'string'
+  return typeof userId !== 'string'
 }
