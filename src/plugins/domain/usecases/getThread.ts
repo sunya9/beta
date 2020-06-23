f4ece5abfacb2b31e906325a9cdbf9a08a596aa3
sunya9 <mugen.xyz@gmail.com>
2020-06-24 05:33:22 +0900

    refactor: move getHtmlMeta

--- src/plugins/domain/usecases/getThread.ts
+++ src/plugins/domain/usecases/getThread.ts
@@ -4,7 +4,7 @@ import { Usecase } from '~/plugins/domain/usecases/usecase'
 import { PnutRepository } from '~/plugins/domain/repository/pnutRepository'
 import { GeneralPostParameters } from '~/plugins/domain/dto/post'
 import { Pagination } from '~/plugins/domain/dto/common'
-import { getImageURLs } from '~/assets/ts/util'
+import { getHtmlMeta } from '~/plugins/domain/util/postUtil'
 
 interface Input {
   postId: string
@@ -15,64 +15,6 @@ interface Output {
   title?: string
   listInfo: ListInfo<Post>
   meta?: any[] // TODO?
-}
-
-function getHtmlMeta(post?: Post) {
-  if (!post || !post.user || !post.content) return
-  const name = post.user.name
-    ? `${post.user.name}(@${post.user.username})`
-    : `@${post.user?.username}`
-  const fullTitle = `${name}: ${post.content?.text}`
-  const title =
-    fullTitle.length > 50 ? `${fullTitle.substr(0, 50)}…` : fullTitle
-  const meta = [
-    { hid: 'description', name: 'description', content: fullTitle },
-    {
-      hid: 'og:description',
-      property: 'og:description',
-      content: fullTitle,
-    },
-    { hid: 'og:title', property: 'og:title', content: title },
-  ]
-  const [photo] = getImageURLs(post, true)
-  if (photo) {
-    meta.push(
-      {
-        hid: 'og:image',
-        property: 'og:image',
-        content: photo.original,
-      },
-      {
-        hid: 'og:image:width',
-        property: 'og:image:width',
-        content: photo.width.toString(),
-      },
-      {
-        hid: 'og:image:height',
-        property: 'og:image:height',
-        content: photo.height.toString(),
-      },
-      {
-        hid: 'og:type',
-        property: 'og:type',
-        content: 'article',
-      },
-      {
-        hid: 'article:published_time',
-        property: 'article:published_time',
-        content: post.created_at.toString(),
-      },
-      {
-        hid: 'article:author',
-        property: 'article:author',
-        content: post.user?.username || '',
-      }
-    )
-  }
-  return {
-    title,
-    meta,
-  }
 }
 
 export interface GetThreadUseCase extends Usecase<Input, Promise<Output>> {}
---
+++ src/plugins/domain/util/postUtil.ts
@@ -0,0 +1,60 @@
+import { Post } from '~/models/post'
+import { getImageURLs } from '~/assets/ts/util'
+
+export function getHtmlMeta(post?: Post) {
+  if (!post || !post.user || !post.content) return
+  const name = post.user.name
+    ? `${post.user.name}(@${post.user.username})`
+    : `@${post.user?.username}`
+  const fullTitle = `${name}: ${post.content?.text}`
+  const title =
+    fullTitle.length > 50 ? `${fullTitle.substr(0, 50)}…` : fullTitle
+  const meta = [
+    { hid: 'description', name: 'description', content: fullTitle },
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: fullTitle,
+    },
+    { hid: 'og:title', property: 'og:title', content: title },
+  ]
+  const [photo] = getImageURLs(post, true)
+  if (photo) {
+    meta.push(
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: photo.original,
+      },
+      {
+        hid: 'og:image:width',
+        property: 'og:image:width',
+        content: photo.width.toString(),
+      },
+      {
+        hid: 'og:image:height',
+        property: 'og:image:height',
+        content: photo.height.toString(),
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'article',
+      },
+      {
+        hid: 'article:published_time',
+        property: 'article:published_time',
+        content: post.created_at.toString(),
+      },
+      {
+        hid: 'article:author',
+        property: 'article:author',
+        content: post.user?.username || '',
+      }
+    )
+  }
+  return {
+    title,
+    meta,
+  }
+}
