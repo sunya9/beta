1697ab65ff7aeb2ed9d018611bcf0a0e394802ba
sunya9 <mugen.xyz@gmail.com>
2020-06-25 05:56:23 +0900

    refactor: split per responsibility

--- src/plugins/di/bind.ts
+++ src/plugins/di/bind.ts
@@ -1,6 +1,5 @@
-import 'reflect-metadata'
-import { Plugin, Context } from '@nuxt/types'
-import { container, inject, singleton } from 'tsyringe'
+import { container } from 'tsyringe'
+import { Context } from '@nuxt/types'
 import {
   CreateFileInteractor,
   CreateFileUseCase,
@@ -92,7 +91,7 @@ import {
 } from '~/plugins/domain/usecases/getRevision'
 import { PnutRepository } from '~/plugins/domain/repository/pnutRepository'
 
-function bind(context: Context) {
+export function bind(context: Context) {
   container
     .register(PnutRepository.token, {
       useValue: new PnutRepositoryImpl(context.$axios),
@@ -129,66 +128,4 @@ function bind(context: Context) {
     .register(GetPollsUseCase.token, { useClass: GetPollsInteractor })
     .register(GetPollUseCase.token, { useClass: GetPollInteractor })
     .register(GetRevisionUseCase.token, { useClass: GetRevisionInteractor })
-}
-
-@singleton()
-class Interactors {
-  constructor(
-    @inject(CreateFileUseCase.token) readonly createFile: CreateFileUseCase,
-    @inject(CreatePollUseCase.token) readonly createPoll: CreatePollUseCase,
-    @inject(GetFileUseCase.token) readonly getFile: GetFileUseCase,
-    @inject(CreatePostUseCase.token) readonly createPost: CreatePostUseCase,
-    @inject(UpdatePostUseCase.token) readonly updatePost: UpdatePostUseCase,
-    @inject(CreateMessageUseCase.token)
-    readonly createMessage: CreateMessageUseCase,
-    @inject(CreateChannelUseCase.token)
-    readonly createChannel: CreateChannelUseCase,
-    @inject(CreatePrivateChannelUseCase.token)
-    readonly createPrivateChannel: CreatePrivateChannelUseCase,
-    @inject(SuggestUsersUseCase.token)
-    readonly suggestUsers: SuggestUsersUseCase,
-    @inject(GetMessagesUseCase.token) readonly getMessages: GetMessagesUseCase,
-    @inject(GetPostsUseCase.token) readonly getPosts: GetPostsUseCase,
-    @inject(GetProfileWithPostsUseCase.token)
-    readonly getProfileWithPosts: GetProfileWithPostsUseCase,
-    @inject(GetInteractionsUseCase.token)
-    readonly getInteractions: GetInteractionsUseCase,
-    @inject(GetUsersUseCase.token) readonly getUsers: GetUsersUseCase,
-    @inject(GetChannelsUseCase.token) readonly getChannels: GetChannelsUseCase,
-    @inject(GetStatsUseCase.token) readonly getStats: GetStatsUseCase,
-    @inject(SearchUseCase.token) readonly search: SearchUseCase,
-    @inject(GetThreadUseCase.token) readonly getThread: GetThreadUseCase,
-    @inject(GetFilesUseCase.token) readonly getFiles: GetFilesUseCase,
-    @inject(GetPollsUseCase.token) readonly getPolls: GetPollsUseCase,
-    @inject(GetPollUseCase.token) readonly getPoll: GetPollUseCase,
-    @inject(GetRevisionUseCase.token) readonly getRevision: GetRevisionUseCase
-  ) {}
-}
-
-const plugin: Plugin = (context, inject) => {
-  bind(context)
-  const interactors = container.resolve(Interactors)
-  inject('interactors', interactors)
-}
-
-type InteractorType = Pick<Interactors, keyof Interactors>
-
-export default plugin
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $interactors: InteractorType
-  }
-}
-
-declare module '@nuxt/types' {
-  interface NuxtAppOptions {
-    $interactors: InteractorType
-  }
-}
-
-declare module 'vuex/types/index' {
-  interface Store<S> {
-    $interactors: InteractorType
-  }
 }
---
+++ src/plugins/di/interactors.ts
@@ -0,0 +1,57 @@
+import { inject, singleton } from 'tsyringe'
+import { CreateFileUseCase } from '~/plugins/domain/usecases/createFile'
+import { CreatePollUseCase } from '~/plugins/domain/usecases/createPoll'
+import { GetFileUseCase } from '~/plugins/domain/usecases/getFile'
+import { CreatePostUseCase } from '~/plugins/domain/usecases/createPost'
+import { UpdatePostUseCase } from '~/plugins/domain/usecases/updatePost'
+import { CreateMessageUseCase } from '~/plugins/domain/usecases/createMessage'
+import { CreateChannelUseCase } from '~/plugins/domain/usecases/createChannel'
+import { CreatePrivateChannelUseCase } from '~/plugins/domain/usecases/createPrivateChannel'
+import { SuggestUsersUseCase } from '~/plugins/domain/usecases/suggestUsers'
+import { GetMessagesUseCase } from '~/plugins/domain/usecases/getMessages'
+import { GetPostsUseCase } from '~/plugins/domain/usecases/getPosts'
+import { GetProfileWithPostsUseCase } from '~/plugins/domain/usecases/getProfileWithPosts'
+import { GetInteractionsUseCase } from '~/plugins/domain/usecases/getInteractions'
+import { GetUsersUseCase } from '~/plugins/domain/usecases/getUsers'
+import { GetChannelsUseCase } from '~/plugins/domain/usecases/getChannels'
+import { GetStatsUseCase } from '~/plugins/domain/usecases/getStats'
+import { SearchUseCase } from '~/plugins/domain/usecases/search'
+import { GetThreadUseCase } from '~/plugins/domain/usecases/getThread'
+import { GetFilesUseCase } from '~/plugins/domain/usecases/getFiles'
+import { GetPollsUseCase } from '~/plugins/domain/usecases/getPolls'
+import { GetPollUseCase } from '~/plugins/domain/usecases/getPoll'
+import { GetRevisionUseCase } from '~/plugins/domain/usecases/getRevision'
+
+@singleton()
+export class Interactors {
+  constructor(
+    @inject(CreateFileUseCase.token) readonly createFile: CreateFileUseCase,
+    @inject(CreatePollUseCase.token) readonly createPoll: CreatePollUseCase,
+    @inject(GetFileUseCase.token) readonly getFile: GetFileUseCase,
+    @inject(CreatePostUseCase.token) readonly createPost: CreatePostUseCase,
+    @inject(UpdatePostUseCase.token) readonly updatePost: UpdatePostUseCase,
+    @inject(CreateMessageUseCase.token)
+    readonly createMessage: CreateMessageUseCase,
+    @inject(CreateChannelUseCase.token)
+    readonly createChannel: CreateChannelUseCase,
+    @inject(CreatePrivateChannelUseCase.token)
+    readonly createPrivateChannel: CreatePrivateChannelUseCase,
+    @inject(SuggestUsersUseCase.token)
+    readonly suggestUsers: SuggestUsersUseCase,
+    @inject(GetMessagesUseCase.token) readonly getMessages: GetMessagesUseCase,
+    @inject(GetPostsUseCase.token) readonly getPosts: GetPostsUseCase,
+    @inject(GetProfileWithPostsUseCase.token)
+    readonly getProfileWithPosts: GetProfileWithPostsUseCase,
+    @inject(GetInteractionsUseCase.token)
+    readonly getInteractions: GetInteractionsUseCase,
+    @inject(GetUsersUseCase.token) readonly getUsers: GetUsersUseCase,
+    @inject(GetChannelsUseCase.token) readonly getChannels: GetChannelsUseCase,
+    @inject(GetStatsUseCase.token) readonly getStats: GetStatsUseCase,
+    @inject(SearchUseCase.token) readonly search: SearchUseCase,
+    @inject(GetThreadUseCase.token) readonly getThread: GetThreadUseCase,
+    @inject(GetFilesUseCase.token) readonly getFiles: GetFilesUseCase,
+    @inject(GetPollsUseCase.token) readonly getPolls: GetPollsUseCase,
+    @inject(GetPollUseCase.token) readonly getPoll: GetPollUseCase,
+    @inject(GetRevisionUseCase.token) readonly getRevision: GetRevisionUseCase
+  ) {}
+}
