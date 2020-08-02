d84e3dba9c02f62da2c2b96d5b80eec9cc6f7074
sunya9 <mugen.xyz@gmail.com>
2020-08-03 06:12:17 +0900

    refactor: use getProfile usecase

--- src/pages/settings/index.vue
+++ src/pages/settings/index.vue
@@ -6,15 +6,18 @@
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import Account from '~/components/molecules/settings/Account/index.vue'
-import { PnutResponse } from '~/entity/pnut-response'
 import { User } from '~/entity/user'
 
 @Component({
   components: {
     Account,
   },
-  async asyncData({ app: { $axios } }) {
-    const { data: account } = await $axios.$get<PnutResponse<User>>('/users/me')
+  async asyncData({ app: { $interactors } }) {
+    const {
+      user: { data: account },
+    } = await $interactors.getProfile.run({
+      username: 'me',
+    })
     return {
       account,
     }
