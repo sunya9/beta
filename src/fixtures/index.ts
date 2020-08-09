e965ddf12980c5dc2754473437db9f8eb568c127
sunya9 <mugen.xyz@gmail.com>
2020-08-09 21:46:49 +0900

    refactor: fixtures

--- src/fixtures/index.ts
+++ src/fixtures/index.ts
@@ -1,13 +1,17 @@
-import { Factory } from 'rosie'
-import '~/fixtures/user'
-import { User } from '~/entity/user'
-
-type FixtureType = 'user'
+import { poll } from '~/fixtures/poll'
+import { user } from '~/fixtures/user'
+import { client } from '~/fixtures/client'
 
 const typeMap = {
-  user: Factory.build<User>('user'),
+  user,
+  poll,
+  client,
 } as const
 
 export function getFixtures<T extends keyof typeof typeMap>(fixtureType: T) {
-  return typeMap[fixtureType]
+  return typeMap[fixtureType].build()
+}
+
+export function buildFixtures<T extends keyof typeof typeMap>(fixtureType: T) {
+  return typeMap[fixtureType].build
 }
--- src/fixtures/user.ts
+++ src/fixtures/user.ts
@@ -1,60 +1,58 @@
 import { Factory } from 'rosie'
 import { User } from '~/entity/user'
 
-Factory.define<User>('user')
-  .sequence('id')
-  .attrs({
-    content: {
-      avatar_image: {
-        height: 256,
-        width: 256,
-        is_default: false,
-        link: 'https://via.placeholder.com/256',
-      },
-      cover_image: {
-        width: 960,
-        height: 223,
-        is_default: false,
-        link: 'https://via.placeholder.com/960x223',
-      },
-      entities: {
-        links: [
-          {
-            amended_len: 18,
-            len: 4,
-            link: 'https://example.com',
-            pos: 0,
-            text: 'test [example.com]',
-          },
-        ],
-        mentions: [],
-        tags: [],
-      },
-      html:
-        '<span itemscope itemtype="https://pnut.io/schemas/Post"><a href="https://example.com">test</a> [example.com]</span>',
-      text: 'test [example.com]',
+export const user = new Factory<User>().sequence('id').attrs({
+  content: {
+    avatar_image: {
+      height: 256,
+      width: 256,
+      is_default: false,
+      link: 'https://via.placeholder.com/256',
     },
-    badge: {
-      id: '1',
-      name: 'farmer',
+    cover_image: {
+      width: 960,
+      height: 223,
+      is_default: false,
+      link: 'https://via.placeholder.com/960x223',
     },
-    counts: {
-      bookmarks: 1,
-      clients: 2,
-      followers: 3,
-      following: 4,
-      posts: 5,
-      users: 6,
+    entities: {
+      links: [
+        {
+          amended_len: 18,
+          len: 4,
+          link: 'https://example.com',
+          pos: 0,
+          text: 'test [example.com]',
+        },
+      ],
+      mentions: [],
+      tags: [],
     },
-    created_at: new Date('2020-01-01'),
-    username: 'test',
-    follows_you: true,
-    you_blocked: false,
-    you_follow: true,
-    you_muted: false,
-    you_can_follow: false,
-    locale: 'ja_JP',
-    name: 'test',
-    timezone: 'Asia/Tokyo',
-    type: User.UserType.human,
-  })
+    html:
+      '<span itemscope itemtype="https://pnut.io/schemas/Post"><a href="https://example.com">test</a> [example.com]</span>',
+    text: 'test [example.com]',
+  },
+  badge: {
+    id: '1',
+    name: 'farmer',
+  },
+  counts: {
+    bookmarks: 1,
+    clients: 2,
+    followers: 3,
+    following: 4,
+    posts: 5,
+    users: 6,
+  },
+  created_at: new Date('2020-01-01'),
+  username: 'test',
+  follows_you: true,
+  you_blocked: false,
+  you_follow: true,
+  you_muted: false,
+  you_can_follow: false,
+  locale: 'ja_JP',
+  name: 'test',
+  timezone: 'Asia/Tokyo',
+  type: User.UserType.human,
+})
