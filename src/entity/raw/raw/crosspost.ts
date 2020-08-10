cbdd0b9b80f837bc9035ab8b0abd5f20dc41b24b
sunya9 <mugen.xyz@gmail.com>
2020-08-11 04:08:32 +0900

    refactor: move getCrosspostLink

--- src/entity/raw/raw/crosspost.ts
+++ src/entity/raw/raw/crosspost.ts
@@ -1,9 +1,17 @@
-import { BaseRaw } from '~/entity/raw'
+import { BaseRaw, Raw, HasRaw } from '~/entity/raw'
+import { Entity } from '~/entity/entity'
 
 export namespace Crosspost {
   export const type = 'io.pnut.core.crosspost' as const
   export interface Value {
     canonical_url: string
+  }
+  export function rawIsCrosspost(raw: Raw): raw is Crosspost {
+    return raw.type === Crosspost.type
+  }
+
+  export function getCrosspostLink(post: HasRaw & Entity.HaveEntityWrapper) {
+    return post.raw?.find(rawIsCrosspost)?.value.canonical_url
   }
 }
 
